04f08bafbcbeabf5b318bf33008fbb192e414e14
Drake Foltz <GeenDutchman@byu.net>
2019-10-24 13:20:45 -0600

    Corrected format of HelpMessage

--- server/src/com/lhf/messages/out/HelpMessage.java
+++ server/src/com/lhf/messages/out/HelpMessage.java
@@ -5,12 +5,12 @@ public class HelpMessage extends OutMessage {
         StringBuilder sb = new StringBuilder("Some commands that you can use:\n");
         sb.append("say [message]\twill tell everyone in your current room the message\n");
         sb.append("tell [username] [message]\twill tell that specific user your message\n");
-        sb.append("look\nwill give you some information about your surroundings\n");
-        sb.append("examine\nwill tell you about some things about interactable objects\n");
+        sb.append("look\twill give you some information about your surroundings\n");
+        sb.append("examine\twill tell you about some things about interactable objects\n");
 
 
         //lastly
-        sb.append("exit\nwill let you disconnect and leave Ibaif");
+        sb.append("exit\twill let you disconnect and leave Ibaif");
         return sb.toString();
     }
 }
