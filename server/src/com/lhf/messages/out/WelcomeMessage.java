95201517557f5d38276aa3b1613fe4b4f2f95f92
Spencer Hastings <1spencer.hastings@gmail.com>
2019-10-25 01:37:20 -0600

    added in some tags to try with the client

--- server/src/com/lhf/messages/out/WelcomeMessage.java
+++ server/src/com/lhf/messages/out/WelcomeMessage.java
@@ -2,11 +2,11 @@ package com.lhf.messages.out;
 
 public class WelcomeMessage extends OutMessage {
     public String toString() {
-        StringBuilder sb = new StringBuilder("Welcome to LHF MUD!\n");
-        sb.append("This is an old-school text-based adventure where multiple users can interact as they trawl the Dungeons of Ibaif!\n");
+        StringBuilder sb = new StringBuilder("Welcome to <title>LHF MUD</title>!\n");
+        sb.append("<description>This is an old-school text-based adventure where multiple users can interact as they trawl the Dungeons of Ibaif!</description>\n");
         sb.append("If you wish to have fun with us, either log on or create a user:\n");
-        sb.append("To create a user, use the command 'create <username> <password>'\n");
-        sb.append("If you wish to leave at any time, simply type 'exit'");
+        sb.append("To create a user, use the command '<command>create <username> <password></command>'\n");
+        sb.append("If you wish to leave at any time, simply type '<command>exit</command>'");
         return sb.toString();
     }
 }
