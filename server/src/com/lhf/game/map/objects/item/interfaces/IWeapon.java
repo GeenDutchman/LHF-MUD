07fbe9c0ebb1c79d481943f9b7a59dbcc4e32309
Drake Foltz <GeenDutchman@byu.net>
2019-10-26 23:46:20 -0600

    Made a basic idea of an Attack (too soon?) Also more on what a Weapon is.

--- server/src/com/lhf/game/map/objects/item/interfaces/IWeapon.java
+++ server/src/com/lhf/game/map/objects/item/interfaces/IWeapon.java
@@ -1,7 +1,13 @@
 package com.lhf.game.map.objects.item.interfaces;
 
+import com.lhf.game.Attack;
+
 public interface IWeapon extends IEquipable {
     WeaponSubtype getWeaponSubtype();
 
     int rollAttack();
+
+    int getDamage();
+
+    Attack getAttack();
 }
