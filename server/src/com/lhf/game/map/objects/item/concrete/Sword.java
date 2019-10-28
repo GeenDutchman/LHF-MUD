490d4b9072a83524d4848bbd4126e72001188b5c
Drake Foltz <GeenDutchman@byu.net>
2019-10-28 06:52:06 -0600

    A simple sword.
    Also decided that you should be able to look at what you equip.

---
+++ server/src/com/lhf/game/map/objects/item/concrete/Sword.java
@@ -0,0 +1,56 @@
+package com.lhf.game.map.objects.item.concrete;
+
+import com.lhf.game.Attack;
+import com.lhf.game.Dice;
+import com.lhf.game.map.objects.item.interfaces.EquipType;
+import com.lhf.game.map.objects.item.interfaces.IWeapon;
+import com.lhf.game.map.objects.item.interfaces.WeaponSubtype;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sword implements IWeapon {
+    @Override
+    public WeaponSubtype getWeaponSubtype() {
+        return WeaponSubtype.SIMPLE;
+    }
+
+    @Override
+    public int rollToHit() {
+        return Dice.roll(1, 20);
+    }
+
+    @Override
+    public int rollDamage() {
+        return Dice.roll(1, 6);
+    }
+
+    @Override
+    public Attack rollAttack() {
+        return new Attack(this.rollToHit()).addFlavorAndDamage("Slashing---", this.rollDamage());
+    }
+
+    @Override
+    public EquipType getType() {
+        return EquipType.WEAPON;
+    }
+
+    @Override
+    public List<Pair<String, Integer>> equip() {
+        return new ArrayList<>(); // changes nothing
+    }
+
+    @Override
+    public List<Pair<String, Integer>> unequip() {
+        return new ArrayList<>(); // changes nothing
+    }
+
+    @Override
+    public String getDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("This is a nice, shiny sword.  It's a bit simple though...");
+        //TODO: should this describe that it does 1d6 damage?
+        return sb.toString();
+    }
+}
--- server/src/com/lhf/game/map/objects/item/interfaces/IEquipable.java
+++ server/src/com/lhf/game/map/objects/item/interfaces/IEquipable.java
@@ -1,10 +1,11 @@
 package com.lhf.game.map.objects.item.interfaces;
 
+import com.lhf.game.map.objects.sharedinterfaces.Examinable;
 import javafx.util.Pair;
 
 import java.util.List;
 
-public interface IEquipable {
+public interface IEquipable extends Examinable {
     EquipType getType();
 
     List<Pair<String, Integer>> equip();
