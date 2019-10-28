29f2ac10982496b7ce5da82750375ab063551496
Drake Foltz <GeenDutchman@byu.net>
2019-10-28 08:06:38 -0600

    A simple armor implementation.
    I don't know if we want to have armor SET the AC or
    INCREMENT the AC.
    
    Shield I think should increment.

--- server/src/com/lhf/game/map/objects/item/concrete/LeatherArmor.java
+++ server/src/com/lhf/game/map/objects/item/concrete/LeatherArmor.java
@@ -7,32 +7,32 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shield implements IEquipable {
+public class LeatherArmor implements IEquipable {
     private int AC = 2;
 
     @Override
     public EquipType getType() {
-        return EquipType.HAND;
+        return EquipType.BODY;
     }
 
     @Override
     public List<Pair<String, Integer>> equip() {
-        ArrayList result = new ArrayList<Pair>();
+        ArrayList result = new ArrayList<Pair<String, Integer>>();
         result.add(new Pair<String, Integer>("AC", this.AC));
         return result;
     }
 
     @Override
     public List<Pair<String, Integer>> unequip() {
-        ArrayList result = new ArrayList<>();
+        ArrayList result = new ArrayList<Pair<String, Integer>>();
         result.add(new Pair<String, Integer>("AC", -1 * this.AC));
         return result;
     }
 
     @Override
     public String getDescription() {
-        StringBuilder sb = new StringBuilder("This is a simple shield, it should protect you a little bit.");
-        // tell how much it boosts player?
+        StringBuilder sb = new StringBuilder("This is some simple leather armor.\n");
+        sb.append("There is only a little blood on it...");
         return sb.toString();
     }
 }
