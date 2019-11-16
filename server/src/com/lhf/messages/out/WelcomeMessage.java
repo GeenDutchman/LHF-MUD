6e8c5bc8432c4bf0706ad13220359bedd2219080
dnajarro <daniel.alonzo.najarro@gmail.com>
2019-11-16 00:30:07 -0700

    formatting in help message

--- server/src/com/lhf/messages/out/WelcomeMessage.java
+++ server/src/com/lhf/messages/out/WelcomeMessage.java
@@ -2,7 +2,7 @@ package com.lhf.messages.out;
 
 public class WelcomeMessage extends OutMessage {
     public String toString() {
-        StringBuilder sb = new StringBuilder("Welcome to <title>LHF MUD</title>!\n\r");
+        StringBuilder sb = new StringBuilder("Welcome to <title>LHF MUD</title>!\r\n");
         sb.append("<description>This is an old-school text-based adventure where multiple users can interact as they trawl the Dungeons of Ibaif!</description>\r\n");
         sb.append("If you wish to have fun with us, either log on or create a user:\r\n");
         sb.append("To create a user, use the command '<command>create <username> <password></command>'\r\n");
