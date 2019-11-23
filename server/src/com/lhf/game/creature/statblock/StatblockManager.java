ac7580f6955b5e637c261c1cb1fcebd6654ddfe4
Drake Foltz <GeenDutchman@byu.net>
2019-11-23 14:32:53 -0700

    Should be working now

--- server/src/com/lhf/game/creature/statblock/StatblockManager.java
+++ server/src/com/lhf/game/creature/statblock/StatblockManager.java
@@ -1,6 +1,7 @@
 package com.lhf.game.creature.statblock;
 
 import java.io.*;
+import java.nio.file.Paths;
 
 public class StatblockManager {
     private BufferedWriter writer;
@@ -11,15 +12,19 @@ public class StatblockManager {
         for (String part : path_to_monsterStatblocks) {
             path.append(part).append(File.separator);
         }
+        System.out.println("Using directory " + Paths.get(".").toAbsolutePath().normalize().toString());
+        System.out.println(path.toString());
     }
 
 
     public void statblockToFile(Statblock statblock){
         try {
+            System.out.println("Using write file: " + path.toString() + statblock.getName());
             writer = new BufferedWriter(new FileWriter(path.toString() + statblock.getName()));
             writer.write(statblock.toString());
             writer.close();
         } catch (IOException e) {
+            System.err.println("Error writing file");
             e.printStackTrace();
         }
     }
@@ -27,10 +32,12 @@ public class StatblockManager {
     public String statblockFromfile(String name){
         char[]file_contents = new char[2048];
         try {
+            System.out.println("Using read file: " + path.toString() + name);
             reader = new BufferedReader(new FileReader(path.toString() + name));
             reader.read(file_contents,0,2048);
             reader.close();
         } catch (IOException e) {
+            System.err.println("Error loading file");
             e.printStackTrace();
         }
 
