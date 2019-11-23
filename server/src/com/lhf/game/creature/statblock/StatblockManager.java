1970ef19758f6a02d95edbfe42a5ec8084bf80a4
Drake Foltz <GeenDutchman@byu.net>
2019-11-23 13:33:09 -0700

    Preliminary try

--- server/src/com/lhf/game/creature/statblock/StatblockManager.java
+++ server/src/com/lhf/game/creature/statblock/StatblockManager.java
@@ -3,15 +3,20 @@ package com.lhf.game.creature.statblock;
 import java.io.*;
 
 public class StatblockManager {
-    public StatblockManager() {
-    }
     private BufferedWriter writer;
     private BufferedReader reader;
-    private String path_to_monsterStatblocks =".\\src\\com\\lhf\\game\\creature\\monsterStatblocks\\";
+    private String[] path_to_monsterStatblocks = {".", "src", "com", "lhf", "game", "creature", "monsterStatblocks"};
+    private StringBuilder path = new StringBuilder();
+    public StatblockManager() {
+        for (String part : path_to_monsterStatblocks) {
+            path.append(part).append(File.separator);
+        }
+    }
+
 
     public void statblockToFile(Statblock statblock){
         try {
-            writer = new BufferedWriter(new FileWriter(path_to_monsterStatblocks+statblock.getName()));
+            writer = new BufferedWriter(new FileWriter(path.toString() + statblock.getName()));
             writer.write(statblock.toString());
             writer.close();
         } catch (IOException e) {
@@ -22,7 +27,7 @@ public class StatblockManager {
     public String statblockFromfile(String name){
         char[]file_contents = new char[2048];
         try {
-            reader = new BufferedReader(new FileReader(path_to_monsterStatblocks+name));
+            reader = new BufferedReader(new FileReader(path.toString() + name));
             reader.read(file_contents,0,2048);
             reader.close();
         } catch (IOException e) {
