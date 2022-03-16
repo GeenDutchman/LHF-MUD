edc334fc8972d393e4b945b6a819928fdf5734d7
Drake Foltz <GeenDutchman@byu.net>
2022-03-15 19:53:05 -0600

    made constant flavors

---
+++ server/src/com/lhf/game/enums/DamageFlavor.java
@@ -0,0 +1,24 @@
+package com.lhf.game.enums;
+
+import com.lhf.game.map.objects.sharedinterfaces.Taggable;
+
+public enum DamageFlavor implements Taggable {
+    SLASHING, MAGICAL_SLASHING, BLUDGEONING, MAGICAL_BLUDGEONING, PIERCING, MAGICAL_PIERCING,
+    FORCE, FIRE, COLD, HEALING, NECROTIC, POISON, ACID, LIGHTNING, THUNDER, PSYCHIC;
+
+    @Override
+    public String getStartTagName() {
+        return "<dmgFlavor>";
+    }
+
+    @Override
+    public String getEndTagName() {
+        return "</dmgFlavor>";
+    }
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase().replace('_', ' ');
+    }
+
+}
