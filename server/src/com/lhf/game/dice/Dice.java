c6759a46783cb65a753c163fa1740a5f4254fac5
Drake Foltz <GeenDutchman@byu.net>
2022-03-18 01:00:51 -0600

    Added RollResult

--- server/src/com/lhf/game/dice/Dice.java
+++ server/src/com/lhf/game/dice/Dice.java
@@ -6,6 +6,43 @@ public abstract class Dice implements Taggable {
     protected int count;
     protected DieType type;
 
+    public class RollResult implements Taggable {
+        protected int total;
+        protected String result;
+
+        public RollResult(int total, String result) {
+            this.total = total;
+            this.result = result;
+        }
+
+        public RollResult combine(RollResult other) {
+            this.total += other.total;
+            this.result = this.result + '+' + other.result;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return this.result + " (" + String.valueOf(this.total) + ")";
+        }
+
+        @Override
+        public String getStartTagName() {
+            return Dice.this.getStartTagName();
+        }
+
+        @Override
+        public String getEndTagName() {
+            return Dice.this.getEndTagName();
+        }
+
+        @Override
+        public String getColorTaggedName() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+    }
+
     public Dice(int count, DieType type) {
         this.count = count;
         this.type = type;
