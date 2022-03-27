a5158e4cd24af832976684002ea0500484d2cd78
Drake Foltz <GeenDutchman@byu.net>
2022-03-26 23:07:05 -0600

    Corpse is container

--- server/src/com/lhf/game/item/concrete/Corpse.java
+++ server/src/com/lhf/game/item/concrete/Corpse.java
@@ -1,7 +1,11 @@
 package com.lhf.game.item.concrete;
 
+import java.util.Optional;
+
 import com.lhf.game.Container;
 import com.lhf.game.item.Item;
+
+// TODO: actually use this
 
 public class Corpse extends Item implements Container {
     public Corpse(String name, boolean isVisible) {
@@ -14,14 +18,24 @@ public class Corpse extends Item implements Container {
     }
 
     @Override
-    public Item takeItem(String name) {
-        return null;
-    }
-
-    @Override
     public String getDescription() {
         return "This is " + this.getColorTaggedName()
                 + ".  They are quite clearly dead.  You can't quite tell the cause...";
+    }
+
+    @Override
+    public Optional<Item> getItem(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Item> removeItem(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean hasItem(String name) {
+        return false;
     }
 
 }
