350bc542dfcdb4e9d71922f48ac44d5cc7e67266
Drake Foltz <GeenDutchman@byu.net>
2022-05-31 23:09:06 -0600

    Start the vocations

---
+++ gameserver/src/main/java/com/lhf/game/creature/vocation/Vocation.java
@@ -0,0 +1,59 @@
+package com.lhf.game.creature.vocation;
+
+import java.util.HashSet;
+
+import com.lhf.Taggable;
+import com.lhf.game.enums.EquipmentTypes;
+
+public abstract class Vocation implements Taggable {
+    private int level;
+    private String name;
+    private HashSet<EquipmentTypes> proficiencies;
+
+    public Vocation(String name) {
+        this.name = name;
+        this.level = 1;
+    }
+
+    public Vocation(String name, HashSet<EquipmentTypes> proficiencies) {
+        this.name = name;
+        this.proficiencies = proficiencies;
+        this.level = 1;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HashSet<EquipmentTypes> getProficiencies() {
+        return proficiencies;
+    }
+
+    public void setProficiencies(HashSet<EquipmentTypes> proficiencies) {
+        this.proficiencies = proficiencies;
+    }
+
+    @Override
+    public String getColorTaggedName() {
+        return this.getStartTagName() + this.getName() + this.getEndTagName();
+    }
+
+    @Override
+    public String getEndTagName() {
+        return "</vocation>";
+    }
+
+    @Override
+    public String getStartTagName() {
+        return "<vocation>";
+    }
+
+}
