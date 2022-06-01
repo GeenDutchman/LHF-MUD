fdbd83a1e43230f5fd871681fc84fcd6be6eb163
Drake Foltz <GeenDutchman@byu.net>
2022-05-31 20:34:18 -0600

    Fixed up welcome message

--- gameserver/src/main/java/com/lhf/messages/out/WelcomeMessage.java
+++ gameserver/src/main/java/com/lhf/messages/out/WelcomeMessage.java
@@ -1,12 +1,18 @@
 package com.lhf.messages.out;
 
+import java.util.StringJoiner;
+
 public class WelcomeMessage extends OutMessage {
     public String toString() {
-        return "Welcome to <title>LHF MUD</title>!\r\n"
-                + "<description>This is an old-school text-based adventure where multiple users can interact as they trawl the Dungeons of Ibaif!</description>\r\n"
-                +
-                "If you wish to have fun with us, either log on or create a user:\r\n" +
-                "To create a user, use the command '<command>create [username] [password]</command>'\r\n" +
-                "If you wish to leave at any time, simply type '<command>exit</command>'\r\n";
+        StringJoiner sj = new StringJoiner("\r\n");
+        sj.add("Welcome to <title>LHF MUD</title>!");
+        sj.add("<description>This is an old-school text-based adventure where multiple users can interact as they trawl the Dungeons of Ibaif!</description>");
+        sj.add("If you wish to have fun with us, either log on or create a user.");
+        sj.add("To create a user, use the command:");
+        sj.add("<command>create \"[username]\" with \"[password]\"</command>");
+        sj.add("If you wish to leave at any time, simply type:");
+        sj.add("<command>exit</command>");
+
+        return sj.toString();
     }
 }
