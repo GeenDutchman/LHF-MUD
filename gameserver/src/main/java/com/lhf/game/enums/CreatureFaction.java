4fc00c01e03b2906892e5ea0fe2f90c4504a4a65
Drake Foltz <GeenDutchman@byu.net>
2022-06-04 13:31:31 -0600

    String to Faction

--- gameserver/src/main/java/com/lhf/game/enums/CreatureFaction.java
+++ gameserver/src/main/java/com/lhf/game/enums/CreatureFaction.java
@@ -1,5 +1,18 @@
 package com.lhf.game.enums;
 
 public enum CreatureFaction {
-    PLAYER, MONSTER, NPC, RENEGADE
+    PLAYER, MONSTER, NPC, RENEGADE;
+
+    public static CreatureFaction getFaction(String value) {
+        for (CreatureFaction faction : values()) {
+            if (faction.toString().equalsIgnoreCase(value)) {
+                return faction;
+            }
+        }
+        return null;
+    }
+
+    public static Boolean isCreatureFaction(String value) {
+        return CreatureFaction.getFaction(value) != null;
+    }
 }
