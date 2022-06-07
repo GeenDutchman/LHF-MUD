5a43b95385727b108c35754eb558c8dd46dc3738
Drake Foltz <GeenDutchman@byu.net>
2022-06-06 22:59:14 -0600

    Refactored creature creator

---
+++ gameserver/src/main/java/com/lhf/game/creature/builder/CLIProvider.java
@@ -0,0 +1,267 @@
+package com.lhf.game.creature.builder;
+
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import com.lhf.game.creature.Creature;
+import com.lhf.game.creature.builder.CreatureCreator.CreatorProvider;
+import com.lhf.game.creature.inventory.Inventory;
+import com.lhf.game.creature.statblock.AttributeBlock;
+import com.lhf.game.enums.Attributes;
+import com.lhf.game.enums.CreatureFaction;
+import com.lhf.game.enums.EquipmentSlots;
+import com.lhf.game.enums.EquipmentTypes;
+import com.lhf.game.enums.Stats;
+import com.lhf.game.item.interfaces.Takeable;
+
+public class CLIProvider implements CreatorProvider {
+
+    private CreatureCreator creator;
+    private Scanner input;
+
+    public CLIProvider() {
+        this.input = new Scanner(System.in);
+    }
+
+    @Override
+    public void setCreator(CreatureCreator creator) {
+        this.creator = creator;
+    }
+
+    @Override
+    public CreatureCreator getCreator() {
+        return this.creator;
+    }
+
+    @Override
+    public String buildName() {
+        Boolean valid;
+        String name;
+        do {
+            System.out.print("Welcome to creature creator, please type the creature's name: ");
+            name = this.input.nextLine();
+
+            System.out.print("The name is: " + name + " is this correct?");
+
+            valid = this.validate();
+
+        } while (!valid);
+        return name;
+    }
+
+    @Override
+    public CreatureFaction buildFaction() {
+        Boolean valid;
+        String factionName;
+        CreatureFaction faction;
+        do {
+            System.out.print("Please indicate the creature's faction (NPC/MONSTER): ");
+            factionName = this.input.nextLine();
+            faction = CreatureFaction.getFaction(factionName);
+
+            if (faction != null) {
+                System.out.print("The creature faction is: " + faction.toString() + " is this correct?");
+                valid = this.validate();
+            } else {
+                System.err.println("Invalid Creature faction, restarting from last prompt.");
+                valid = Boolean.FALSE;
+            }
+
+        } while (!valid);
+
+        return faction;
+    }
+
+    @Override
+    public AttributeBlock buildAttributeBlock() {
+        // attributes
+        AttributeBlock attributes = new AttributeBlock();
+        Boolean valid;
+        do {
+            System.out.print(
+                    "Enter the attributes with a space between each number (STR DEX CON INT WIS CHA): ");
+            try {
+                Integer response_int;
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.STR, response_int);
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.DEX, response_int);
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.CON, response_int);
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.INT, response_int);
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.WIS, response_int);
+                response_int = input.nextInt();
+                attributes.setScore(Attributes.CHA, response_int);
+
+            } catch (java.util.InputMismatchException e) {
+                System.err.println("Invalid input, expected 6 integers separated by spaces.");
+                input.nextLine(); // clear buffer
+                valid = Boolean.FALSE;
+                continue;
+            }
+
+            System.out.println(attributes.toString());
+            System.out.print(" is this correct? ");
+            this.input.nextLine(); // clear buffer
+            valid = this.validate();
+        } while (!valid);
+
+        return attributes;
+    }
+
+    @Override
+    public HashMap<Stats, Integer> buildStats() {
+        Boolean valid;
+        HashMap<Stats, Integer> stats = new HashMap<>();
+        do {
+            int max_hp;
+            int xp_worth;
+            float cr;
+            System.out.print(
+                    "What is the max HP (integer) xp worth(integer) and approximate CR (float) (each value should be separated by a space): ");
+            try {
+                max_hp = input.nextInt();
+                xp_worth = input.nextInt();
+                cr = input.nextFloat();
+
+            } catch (java.util.InputMismatchException e) {
+                System.err.println(
+                        "Invalid input, expected two integers and a float (ex float: 0.25 2.0) separated by spaces.");
+                input.nextLine(); // clear buffer
+                valid = Boolean.FALSE;
+                continue;
+            }
+
+            this.input.nextLine(); // clears buffer
+
+            stats.put(Stats.MAXHP, max_hp);
+            stats.put(Stats.CURRENTHP, max_hp);
+
+            stats.put(Stats.XPWORTH, xp_worth);
+            stats.put(Stats.XPEARNED, 0);
+            stats.put(Stats.PROFICIENCYBONUS, 0);
+            // NOTE: 1 CR is roughly 4 level one players
+            stats.put(Stats.AC, 10 + this.getCreator().getAttributes().getMod(Attributes.DEX));
+
+            System.out.print("Given max HP of " + max_hp + " and cr of " + cr +
+                    " max/current hp is: " + stats.get(Stats.CURRENTHP) + "" +
+                    " xp worth is: " + stats.get(Stats.XPWORTH) + " Is this correct?(yes,no) ");
+            valid = this.validate();
+
+        } while (!valid);
+        return stats;
+    }
+
+    @Override
+    public HashSet<EquipmentTypes> buildProficiencies() {
+        String proficiency_string;
+        EquipmentTypes proficiency;
+        HashSet<EquipmentTypes> proficiencies = new HashSet<>();
+        while (true) {
+            System.out.print("Enter one of the proficiencies or 'done' if there are no more to add: ");
+            proficiency_string = this.input.nextLine();
+            if (proficiency_string.equalsIgnoreCase("done")) {
+                break;
+            }
+            try {
+                proficiency = EquipmentTypes.valueOf(proficiency_string.toUpperCase());
+                proficiencies.add(proficiency);
+            } catch (java.lang.IllegalArgumentException e) {
+                System.err.println(proficiency_string
+                        + " is not contained in EquipmentTypes file, try again or come back once you add it to the file.");
+            }
+        }
+
+        return proficiencies;
+    }
+
+    @Override
+    public Inventory buildInventory() {
+        Inventory inventory = new Inventory();
+        String item;
+        // May need to replace this to be more adaptive?
+        String path_to_items = "com.lhf.game.item.concrete.equipment.";
+        while (true) {
+            System.out.print(
+                    "Enter one of their inventory items (including weapons and armor) (FilenameOfItem or done): ");
+            item = input.nextLine();
+            item = item.strip();
+
+            if (item.equalsIgnoreCase("done")) {
+                break;
+            }
+
+            try {
+                Class<?> clazz = Class.forName(path_to_items + item);
+                Constructor<?> constructor = clazz.getConstructor(boolean.class);
+                Object item_instance = constructor.newInstance(Boolean.TRUE);
+                inventory.addItem((Takeable) item_instance);
+
+            } catch (java.lang.NoClassDefFoundError | java.lang.ClassNotFoundException | java.lang.NoSuchMethodException
+                    | java.lang.IllegalAccessException | java.lang.InstantiationException
+                    | java.lang.reflect.InvocationTargetException e) {
+                System.err.println(item + " not found in package " + path_to_items
+                        + " \nPlease enter a valid item class's filename with camelCase and all that jazz.");
+            }
+
+            System.out.println(inventory.toStoreString());
+
+        }
+
+        return inventory;
+    }
+
+    @Override
+    public void equipFromInventory(Creature creature) {
+        String item_slot_string;
+        while (true) {
+            System.out.print("Given: " + creature.getInventory().toStoreString()
+                    + " \nIs there anything you would like to equip?(Item Name,slot or done) ");
+            item_slot_string = input.nextLine().strip();
+            if (item_slot_string.equalsIgnoreCase("done")) {
+                break;
+            }
+            String[] pair = item_slot_string.split(",");
+            try {
+
+                EquipmentSlots slot = EquipmentSlots.valueOf(pair[1].strip().toUpperCase());
+                creature.equipItem(pair[0], slot);
+
+            } catch (java.lang.IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+                System.err.println("Slots are: " + Arrays.toString(EquipmentSlots.values()));
+            } catch (java.lang.ArrayIndexOutOfBoundsException e) {
+                System.err.println("Expected a comma between Item and Slot.");
+            }
+
+        }
+    }
+
+    private Boolean validate() {
+        System.out.println("yes or no?");
+        String validation_response = this.input.nextLine().toLowerCase();
+        if (validation_response.equals("yes") || validation_response.equals("no")) {
+            if (validation_response.equals("yes")) {
+                return Boolean.TRUE;
+            } else {
+                System.out.println("Restarting from last prompt.");
+                return Boolean.FALSE;
+            }
+        } else {
+            System.err.println("Invalid response, restarting from last prompt.");
+            return Boolean.FALSE;
+        }
+
+    }
+
+    @Override
+    public void close() {
+        this.input.close();
+    }
+
+}
--- gameserver/src/main/java/com/lhf/game/creature/builder/CreatureCreator.java
+++ gameserver/src/main/java/com/lhf/game/creature/builder/CreatureCreator.java
@@ -1,5 +1,6 @@
 package com.lhf.game.creature.builder;
 
+import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
@@ -21,233 +22,109 @@ import com.lhf.game.item.Item;
 import com.lhf.game.item.interfaces.Takeable;
 
 public class CreatureCreator {
+    public interface CreatorProvider extends Closeable {
+        public void setCreator(CreatureCreator creator);
 
-    private CreatureCreator() {
+        public CreatureCreator getCreator();
+
+        public String buildName();
+
+        public CreatureFaction buildFaction();
+
+        public AttributeBlock buildAttributeBlock();
+
+        public HashMap<Stats, Integer> buildStats();
+
+        public HashSet<EquipmentTypes> buildProficiencies();
+
+        public Inventory buildInventory();
+
+        public void equipFromInventory(Creature creature);
+
+        public void close();
+    };
+
+    private CreatorProvider provider;
+
+    private String name;
+    private CreatureFaction faction;
+    private AttributeBlock attributes;
+    private HashMap<Stats, Integer> stats;
+    private HashSet<EquipmentTypes> proficiencies;
+    private Inventory inventory;
+    private HashMap<EquipmentSlots, Item> equipSlots;
+
+    private Statblock statblock;
+
+    private CreatureCreator(CreatorProvider provider) {
+        this.provider = provider;
     }
 
-    // private int XP_FROM_CR = 200;
-    // private int BONUS_XP_MOD = 50;
+    public void setProvider(CreatorProvider provider) {
+        this.provider = provider;
+    }
 
-    public String buildName(Scanner input) {
-        Boolean valid;
-        String name;
-        do {
-            System.out.print("Welcome to creature creator, please type the creature's name: ");
-            name = input.nextLine();
-
-            System.out.print("The name is: " + name + " is this correct?");
-
-            valid = validate(input);
-
-        } while (!valid);
+    public String getName() {
         return name;
     }
 
-    public CreatureFaction buildFaction(Scanner input) {
-        Boolean valid;
-        String factionName;
-        CreatureFaction faction;
-        do {
-            System.out.print("Please indicate the creature's faction (NPC/MONSTER): ");
-            factionName = input.nextLine();
-            faction = CreatureFaction.getFaction(factionName);
-
-            if (faction != null) {
-                System.out.print("The creature faction is: " + faction.toString() + " is this correct?");
-                valid = validate(input);
-            } else {
-                System.err.println("Invalid Creature faction, restarting from last prompt.");
-                valid = Boolean.FALSE;
-            }
-
-        } while (!valid);
-
+    public CreatureFaction getFaction() {
         return faction;
     }
 
-    public AttributeBlock buildAttributeBlock(Scanner input) {
-        // attributes
-        AttributeBlock attributes = new AttributeBlock();
-        Boolean valid;
-        do {
-            System.out.print(
-                    "Enter the attributes with a space between each number (STR DEX CON INT WIS CHA): ");
-            try {
-                Integer response_int;
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.STR, response_int);
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.DEX, response_int);
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.CON, response_int);
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.INT, response_int);
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.WIS, response_int);
-                response_int = input.nextInt();
-                attributes.setScore(Attributes.CHA, response_int);
-
-            } catch (java.util.InputMismatchException e) {
-                System.err.println("Invalid input, expected 6 integers separated by spaces.");
-                input.nextLine(); // clear buffer
-                valid = Boolean.FALSE;
-                continue;
-            }
-
-            System.out.println(attributes.toString());
-            System.out.print(" is this correct? ");
-            input.nextLine(); // clear buffer
-            valid = validate(input);
-        } while (!valid);
-
+    public AttributeBlock getAttributes() {
         return attributes;
     }
 
-    public HashMap<Stats, Integer> buildStats(Scanner input, AttributeBlock attributes) {
-        Boolean valid;
-        HashMap<Stats, Integer> stats = new HashMap<>();
-        do {
-            int max_hp;
-            int xp_worth;
-            float cr;
-            System.out.print(
-                    "What is the max HP (integer) xp worth(integer) and approximate CR (float) (each value should be separated by a space): ");
-            try {
-                max_hp = input.nextInt();
-                xp_worth = input.nextInt();
-                cr = input.nextFloat();
-
-            } catch (java.util.InputMismatchException e) {
-                System.err.println(
-                        "Invalid input, expected two integers and a float (ex float: 0.25 2.0) separated by spaces.");
-                input.nextLine(); // clear buffer
-                valid = Boolean.FALSE;
-                continue;
-            }
-
-            input.nextLine(); // clears buffer
-
-            stats.put(Stats.MAXHP, max_hp);
-            stats.put(Stats.CURRENTHP, max_hp);
-
-            stats.put(Stats.XPWORTH, xp_worth);
-            stats.put(Stats.XPEARNED, 0);
-            stats.put(Stats.PROFICIENCYBONUS, 0);
-            // NOTE: 1 CR is roughly 4 level one players
-            stats.put(Stats.AC, 10 + attributes.getMod(Attributes.DEX));
-
-            System.out.print("Given max HP of " + max_hp + " and cr of " + cr +
-                    " max/current hp is: " + stats.get(Stats.CURRENTHP) + "" +
-                    " xp worth is: " + stats.get(Stats.XPWORTH) + " Is this correct?(yes,no) ");
-            valid = validate(input);
-
-        } while (!valid);
+    public HashMap<Stats, Integer> getStats() {
         return stats;
     }
 
-    public HashSet<EquipmentTypes> buildProficiencies(Scanner input) {
-        String proficiency_string;
-        EquipmentTypes proficiency;
-        HashSet<EquipmentTypes> proficiencies = new HashSet<>();
-        while (true) {
-            System.out.print("Enter one of the proficiencies or 'done' if there are no more to add: ");
-            proficiency_string = input.nextLine();
-            if (proficiency_string.equalsIgnoreCase("done")) {
-                break;
-            }
-            try {
-                proficiency = EquipmentTypes.valueOf(proficiency_string.toUpperCase());
-                proficiencies.add(proficiency);
-            } catch (java.lang.IllegalArgumentException e) {
-                System.err.println(proficiency_string
-                        + " is not contained in EquipmentTypes file, try again or come back once you add it to the file.");
-            }
-        }
-
+    public HashSet<EquipmentTypes> getProficiencies() {
         return proficiencies;
     }
 
-    public Inventory buildInventory(Scanner input) {
-        Inventory inventory = new Inventory();
-        String item;
-        // May need to replace this to be more adaptive?
-        String path_to_items = "com.lhf.game.item.concrete.equipment.";
-        while (true) {
-            System.out.print(
-                    "Enter one of their inventory items (including weapons and armor) (FilenameOfItem or done): ");
-            item = input.nextLine();
-            item = item.strip();
-
-            if (item.equalsIgnoreCase("done")) {
-                break;
-            }
-
-            try {
-                Class<?> clazz = Class.forName(path_to_items + item);
-                Constructor<?> constructor = clazz.getConstructor(boolean.class);
-                Object item_instance = constructor.newInstance(Boolean.TRUE);
-                inventory.addItem((Takeable) item_instance);
-
-            } catch (java.lang.NoClassDefFoundError | java.lang.ClassNotFoundException | java.lang.NoSuchMethodException
-                    | java.lang.IllegalAccessException | java.lang.InstantiationException
-                    | java.lang.reflect.InvocationTargetException e) {
-                System.err.println(item + " not found in package " + path_to_items
-                        + " \nPlease enter a valid item class's filename with camelCase and all that jazz.");
-            }
-
-            System.out.println(inventory.toStoreString());
-
-        }
-
+    public Inventory getInventory() {
         return inventory;
     }
 
-    public void setEquipment(Creature creature, Scanner input) {
-        String item_slot_string;
-        while (true) {
-            System.out.print("Given: " + creature.getInventory().toStoreString()
-                    + " \nIs there anything you would like to equip?(Item Name,slot or done) ");
-            item_slot_string = input.nextLine().strip();
-            if (item_slot_string.equalsIgnoreCase("done")) {
-                break;
-            }
-            String[] pair = item_slot_string.split(",");
-            try {
-
-                EquipmentSlots slot = EquipmentSlots.valueOf(pair[1].strip().toUpperCase());
-                creature.equipItem(pair[0], slot);
-
-            } catch (java.lang.IllegalArgumentException e) {
-                System.err.println(e.getMessage());
-                System.err.println("Slots are: " + Arrays.toString(EquipmentSlots.values()));
-            } catch (java.lang.ArrayIndexOutOfBoundsException e) {
-                System.err.println("Expected a comma between Item and Slot.");
-            }
-
-        }
+    public HashMap<EquipmentSlots, Item> getEquipSlots() {
+        return equipSlots;
     }
 
-    private void makeCreature() {
-        Scanner input = new Scanner(System.in);
+    public Statblock getStatblock() {
+        return statblock;
+    }
 
+    public Statblock writeStatblock(Statblock towrite) {
+        StatblockManager loader_unloader = new StatblockManager();
+        loader_unloader.statblockToFile(towrite);
+        try {
+            return loader_unloader.statblockFromfile(name);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Creature makeCreature() {
         // name
-        String name = this.buildName(input);
+        this.name = this.provider.buildName();
 
         // creature faction
-
-        CreatureFaction faction = this.buildFaction(input);
+        this.faction = this.provider.buildFaction();
 
         // attributes
-        AttributeBlock attributes = this.buildAttributeBlock(input);
+        this.attributes = this.provider.buildAttributeBlock();
 
         // stats
-        HashMap<Stats, Integer> stats = this.buildStats(input, attributes);
+        this.stats = this.provider.buildStats();
 
         // Adds proficiencies
-        HashSet<EquipmentTypes> proficiencies = this.buildProficiencies(input);
+        this.proficiencies = this.provider.buildProficiencies();
 
         // Adds items
-        Inventory inventory = this.buildInventory(input);
+        this.inventory = this.provider.buildInventory();
 
         HashMap<EquipmentSlots, Item> equipmentSlots = new HashMap<>();
 
@@ -256,44 +133,23 @@ public class CreatureCreator {
 
         Creature npc = new Creature(name, creation);
 
-        this.setEquipment(npc, input);
+        this.provider.equipFromInventory(npc);
 
-        System.out.print(creation);
+        // System.out.print(creation);
 
         Statblock test = new Statblock(creation.toString());
-        System.out.println(test);
+        // System.out.println(test);
 
-        StatblockManager loader_unloader = new StatblockManager();
-        loader_unloader.statblockToFile(test);
-        try {
-            test = loader_unloader.statblockFromfile(name);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        System.err.println(test);
-        input.close();
-        System.out.println("\nCreature Creation Complete!");
-    }
-
-    private Boolean validate(Scanner input) {
-        System.out.println("yes or no?");
-        String validation_response = input.nextLine().toLowerCase();
-        if (validation_response.equals("yes") || validation_response.equals("no")) {
-            if (validation_response.equals("yes")) {
-                return Boolean.TRUE;
-            } else {
-                System.out.println("Restarting from last prompt.");
-                return Boolean.FALSE;
-            }
-        } else {
-            System.err.println("Invalid response, restarting from last prompt.");
-            return Boolean.FALSE;
-        }
-
+        test = this.writeStatblock(test);
+        // System.err.println(test);
+        this.provider.close();
+        // System.out.println("\nCreature Creation Complete!");
+        return npc;
     }
 
     public static void main(String[] args) {
-        CreatureCreator creator = new CreatureCreator();
+        CLIProvider cliProvider = new CLIProvider();
+        CreatureCreator creator = new CreatureCreator(cliProvider);
         creator.makeCreature();
     }
 }
