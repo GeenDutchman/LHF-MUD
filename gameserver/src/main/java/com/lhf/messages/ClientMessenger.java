87d19c318f049e6d659d3111058d9f05a47c9ce8
Drake Foltz <GeenDutchman@byu.net>
2022-06-25 17:05:26 -0600

    Improve saymessage

--- gameserver/src/main/java/com/lhf/messages/ClientMessenger.java
+++ gameserver/src/main/java/com/lhf/messages/ClientMessenger.java
@@ -1,9 +1,10 @@
 package com.lhf.messages;
 
+import com.lhf.Taggable;
 import com.lhf.messages.out.OutMessage;
 import com.lhf.server.client.ClientID;
 
-public interface ClientMessenger {
+public interface ClientMessenger extends Taggable {
     public void sendMsg(OutMessage msg);
 
     public ClientID getClientID();
--- gameserver/src/main/java/com/lhf/messages/out/SayMessage.java
+++ gameserver/src/main/java/com/lhf/messages/out/SayMessage.java
@@ -1,18 +1,43 @@
 package com.lhf.messages.out;
 
-import com.lhf.server.client.user.User;
+import com.lhf.messages.ClientMessenger;
 
 public class SayMessage extends OutMessage {
     private String message;
-    private User user;
+    private ClientMessenger sayer;
+    private ClientMessenger hearer;
 
-    public SayMessage(String payload, User user) {
-        message = payload;
-        this.user = user;
+    public SayMessage(ClientMessenger sayer, String message) {
+        this.sayer = sayer;
+        this.message = message;
+    }
+
+    public SayMessage(ClientMessenger sayer, String message, ClientMessenger hearer) {
+        this.sayer = sayer;
+        this.message = message;
+        this.hearer = hearer;
     }
 
     @Override
     public String toString() {
-        return user.getColorTaggedUsername() + ": " + message;
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.sayer.getColorTaggedName());
+        if (this.hearer != null) {
+            sb.append(" to ").append(this.hearer.getColorTaggedName());
+        }
+        sb.append(":").append(this.message);
+        return sb.toString();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ClientMessenger getSayer() {
+        return sayer;
+    }
+
+    public ClientMessenger getHearer() {
+        return hearer;
     }
 }
