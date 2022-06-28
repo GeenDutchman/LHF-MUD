ab088e8bdb1107b7289548737dda7dfb104ad64a
Drake Foltz <GeenDutchman@byu.net>
2022-06-27 18:49:44 -0600

    quick note

--- gameserver/src/main/java/com/lhf/game/creature/NonPlayerCharacter.java
+++ gameserver/src/main/java/com/lhf/game/creature/NonPlayerCharacter.java
@@ -3,6 +3,8 @@ package com.lhf.game.creature;
 import com.lhf.game.creature.statblock.Statblock;
 import com.lhf.game.enums.CreatureFaction;
 
+//  TODO:  https://gamedev.stackexchange.com/questions/12458/how-to-manage-all-the-npc-ai-objects-on-the-server/12512#12512
+
 public class NonPlayerCharacter extends Creature {
     public NonPlayerCharacter() {
         super();
