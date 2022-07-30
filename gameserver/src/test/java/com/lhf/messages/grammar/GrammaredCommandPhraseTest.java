53cc1c1736091b0737a43e118bc44917425c7da2
Drake Foltz <GeenDutchman@byu.net>
2022-07-29 21:37:01 -0600

    even more spaces

--- gameserver/src/test/java/com/lhf/messages/grammar/GrammaredCommandPhraseTest.java
+++ gameserver/src/test/java/com/lhf/messages/grammar/GrammaredCommandPhraseTest.java
@@ -12,14 +12,20 @@ public class GrammaredCommandPhraseTest {
     @Test
     void testParse() {
         ArrayList<GrammarTestCase> testcases = new ArrayList<>();
-        testcases.add(new GrammarTestCase("GO east", true).addToken(CommandMessage.GO.toString(), true).addToken("east",
-                true));
-        testcases.add(new GrammarTestCase("GO east", true).addToken("go", true).addToken("east", true));
+        testcases.add(new GrammarTestCase("GO east", true).addToken(CommandMessage.GO.toString(), true)
+                .addToken(" ", true).addToken("east",
+                        true));
+        testcases.add(
+                new GrammarTestCase("GO east", true).addToken("go", true).addToken(" ", true).addToken("east", true));
         testcases.add(new GrammarTestCase("SEE", true).addToken("see", true));
-        testcases.add(new GrammarTestCase("blarch SEE", false).addToken("blarch", false).addToken("see", true));
+        testcases.add(new GrammarTestCase("blarch SEE", false).addToken("blarch", false).addToken(" ", true)
+                .addToken("see", true));
         testcases.add(new GrammarTestCase("SAY 'say hello to my', joe to joe", true).addToken("say", true)
-                .addToken("'", true).addToken("say", true).addToken("hello", true).addToken("to", true)
-                .addToken("my", true).addToken("'", true).addToken(",", true).addToken("joe", true).addToken("to", true)
+                .addToken(" ", true)
+                .addToken("'", true).addToken("say", true).addToken(" ", true).addToken("hello", true)
+                .addToken(" ", true).addToken("to", true).addToken(" ", true)
+                .addToken("my", true).addToken("'", true).addToken(",", true).addToken(" ", true).addToken("joe", true)
+                .addToken(" ", true).addToken("to", true).addToken(" ", true)
                 .addToken("joe", true));
 
         for (GrammarTestCase testcase : testcases) {
