537d199bd685649e69ab3bdc99dc04247ce1df17
Drake Foltz <GeenDutchman@byu.net>
2022-07-29 21:34:29 -0600

    more spaces

--- gameserver/src/test/java/com/lhf/messages/grammar/PhraseListTest.java
+++ gameserver/src/test/java/com/lhf/messages/grammar/PhraseListTest.java
@@ -13,34 +13,43 @@ public class PhraseListTest {
     void testParse() {
         ArrayList<GrammarTestCase> testcases = new ArrayList<>();
         testcases.add(new GrammarTestCase("oneword", true).addToken("oneword", true));
-        testcases.add(new GrammarTestCase("oneword twoword", true).addToken("oneword", true).addToken("twoword",
-                true));
+        testcases.add(new GrammarTestCase("oneword twoword", true).addToken("oneword", true).addToken(" ", true)
+                .addToken("twoword",
+                        true));
         testcases.add(new GrammarTestCase("oneword 'midone midtwo' twoword", true).addToken("oneword", true)
+                .addToken(" ", true)
                 .addToken("'", true)
-                .addToken("midone", true).addToken("midtwo", true).addToken("'", true)
+                .addToken("midone", true).addToken(" ", true).addToken("midtwo", true).addToken("'", true)
+                .addToken(" ", true)
                 .addToken("twoword", true));
-        testcases.add(new GrammarTestCase("oneword 'openquote", false).addToken("oneword", true)
+        testcases.add(new GrammarTestCase("oneword 'openquote", false).addToken("oneword", true).addToken(" ", true)
                 .addToken("'", true)
                 .addToken("openquote", true));
-        testcases.add(new GrammarTestCase("here we go"/* to there" */, true).addToken("here", true)
-                .addToken("we", true)
-                .addToken("go", true).addToken("to", false).addToken("there", false));
+        testcases.add(new GrammarTestCase("here we go"/* to there" */, true).addToken("here", true).addToken(" ", true)
+                .addToken("we", true).addToken(" ", true)
+                .addToken("go", true).addToken(" ", true).addToken("to", false).addToken(" ", true)
+                .addToken("there", false));
         testcases.add(new GrammarTestCase("oneword (midone midtwo) twoword", true).addToken("oneword", true)
+                .addToken(" ", true)
                 .addToken("(", true)
-                .addToken("midone", true).addToken("midtwo", true).addToken(")", true)
+                .addToken("midone", true).addToken(" ", true).addToken("midtwo", true).addToken(")", true)
+                .addToken(" ", true)
                 .addToken("twoword", true));
         testcases.add(
                 new GrammarTestCase("hello, there", true).addToken("hello", true).addToken(",", true)
+                        .addToken(" ", true)
                         .addToken("there",
                                 true));
         testcases.add(new GrammarTestCase("", false).addToken(",", true));
         testcases.add(new GrammarTestCase("", false).addToken(",", true).addToken(",", true).addToken(",",
                 true));
         testcases.add(new GrammarTestCase("I, 'have no', bananas for you", true).addToken("I", true)
-                .addToken(",", true)
+                .addToken(",", true).addToken(" ", true)
                 .addToken("'", true)
-                .addToken("have", true).addToken("no", true).addToken("'", true).addToken(",", true)
-                .addToken("bananas", true).addToken("for", true).addToken("you", true));
+                .addToken("have", true).addToken(" ", true).addToken("no", true).addToken("'", true).addToken(",", true)
+                .addToken(" ", true)
+                .addToken("bananas", true).addToken(" ", true).addToken("for", true).addToken(" ", true)
+                .addToken("you", true));
 
         HashSet<String> preps = new HashSet<>();
         preps.add("to");
