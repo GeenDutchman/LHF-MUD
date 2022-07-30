a6d1e6fbce8ef2261870f8899c593ca928fe532c
Drake Foltz <GeenDutchman@byu.net>
2022-07-30 14:09:13 -0600

    Concept of a doorway

---
+++ gameserver/src/main/java/com/lhf/game/map/Doorway.java
@@ -0,0 +1,28 @@
+package com.lhf.game.map;
+
+import com.lhf.game.creature.Creature;
+
+public abstract class Doorway {
+    public abstract Room getRoomA();
+
+    public abstract Room getRoomB();
+
+    public boolean traverse(Creature creature) {
+
+        Room roomA = this.getRoomA();
+        Room roomB = this.getRoomB();
+
+        if (roomA.containsCreature(creature)) {
+            creature.setSuccessor(roomB);
+            roomB.addCreature(creature);
+            roomA.removeCreature(creature);
+            return true;
+        } else if (roomB.containsCreature(creature)) {
+            creature.setSuccessor(roomA);
+            roomA.addCreature(creature);
+            roomB.removeCreature(creature);
+            return true;
+        }
+        return false;
+    }
+}
