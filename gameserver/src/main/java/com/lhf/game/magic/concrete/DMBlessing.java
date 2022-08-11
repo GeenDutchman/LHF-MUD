008460a6eb4077d7d6550f574b3a66f77e2f206b
Drake Foltz <GeenDutchman@byu.net>
2022-08-11 12:38:27 -0600

    Made the DM's blessing

---
+++ gameserver/src/main/java/com/lhf/game/magic/concrete/DMBlessing.java
@@ -0,0 +1,27 @@
+package com.lhf.game.magic.concrete;
+
+import com.lhf.game.EntityEffector.EffectPersistence;
+import com.lhf.game.creature.vocation.Vocation.VocationName;
+import com.lhf.game.enums.Attributes;
+import com.lhf.game.enums.Stats;
+import com.lhf.game.magic.CreatureTargetingSpellEntry;
+
+public class DMBlessing extends CreatureTargetingSpellEntry {
+
+    public static final String name = "DMBlessing";
+
+    public DMBlessing() {
+        super(10, DMBlessing.name, "I bless you", EffectPersistence.DURATION,
+                "Blesses the target with extra stats and attributes, and does extra stuff for NPC's.",
+                true, false, VocationName.DUNGEON_MASTER);
+
+        this.statChanges.put(Stats.MAXHP, 200);
+        this.statChanges.put(Stats.CURRENTHP, 200);
+        this.statChanges.put(Stats.PROFICIENCYBONUS, 5);
+        this.attributeScoreChanges.put(Attributes.STR, 20);
+        this.attributeScoreChanges.put(Attributes.DEX, 20);
+        this.attributeScoreChanges.put(Attributes.CHA, 20);
+        this.attributeScoreChanges.put(Attributes.CON, 20);
+    }
+
+}
