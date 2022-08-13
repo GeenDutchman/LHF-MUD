1684f5fd45e5ebf81f29a514f0b671f7c53c388f
Drake Foltz <GeenDutchman@byu.net>
2022-08-12 21:57:54 -0600

    too much flexible?

--- gameserver/src/main/java/com/lhf/messages/OutMessagePair.java
+++ gameserver/src/main/java/com/lhf/messages/OutMessagePair.java
@@ -1,26 +1,31 @@
 package com.lhf.messages;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.lhf.messages.out.OutMessage;
 
 public class OutMessagePair {
     public OutMessage toEveryone;
-    public Map<ClientMessenger, OutMessage> directed;
+    public Map<ClientMessenger, List<OutMessage>> directed;
 
     public OutMessagePair(OutMessage toEveryone) {
         this.toEveryone = toEveryone;
         this.directed = new HashMap<>();
     }
 
-    public OutMessagePair(OutMessage toEveryone, Map<ClientMessenger, OutMessage> directed) {
+    public OutMessagePair(OutMessage toEveryone, Map<ClientMessenger, List<OutMessage>> directed) {
         this.toEveryone = toEveryone;
         this.directed = directed;
     }
 
     public OutMessagePair addDirected(ClientMessenger messenger, OutMessage message) {
-        this.directed.put(messenger, message);
+        if (this.directed.containsKey(messenger)) {
+            this.directed.put(messenger, new ArrayList<>());
+        }
+        this.directed.get(messenger).add(message);
         return this;
     }
 }
