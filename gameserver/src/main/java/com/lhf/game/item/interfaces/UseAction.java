6329157684bea8efe71703f38efa7c181ad6579d
Drake Foltz <GeenDutchman@byu.net>
2022-08-14 16:01:16 -0600

    We don't actually need those

--- gameserver/src/main/java/com/lhf/game/item/interfaces/UseAction.java
+++ gameserver/src/main/java/com/lhf/game/item/interfaces/UseAction.java
@@ -1,31 +1,8 @@
 package com.lhf.game.item.interfaces;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.lhf.game.EntityEffector;
 import com.lhf.messages.CommandContext;
-import com.lhf.messages.OutMessagePair;
 
 public interface UseAction {
-    public class UseageResult {
-        public OutMessagePair messages;
-        public List<EntityEffector> effectors;
 
-        public UseageResult(OutMessagePair messages, EntityEffector effector) {
-            this.messages = messages;
-            this.effectors = new ArrayList<>();
-            if (effector != null) {
-                this.effectors.add(effector);
-            }
-        }
-
-        public UseageResult addEffect(EntityEffector effector) {
-            this.effectors.add(effector);
-            return this;
-        }
-
-    }
-
-    UseageResult useAction(CommandContext ctx, Object useOn);
+    boolean useAction(CommandContext ctx, Object useOn);
 }
