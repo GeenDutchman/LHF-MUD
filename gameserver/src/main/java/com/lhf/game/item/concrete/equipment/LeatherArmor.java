211a561446d2f1f571685a5fbf878f3af7915eae
Drake Foltz <GeenDutchman@byu.net>
2022-08-15 18:18:44 -0600

    All the armors done

--- gameserver/src/main/java/com/lhf/game/item/concrete/equipment/LeatherArmor.java
+++ gameserver/src/main/java/com/lhf/game/item/concrete/equipment/LeatherArmor.java
@@ -1,50 +1,31 @@
 package com.lhf.game.item.concrete.equipment;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+import com.lhf.game.EffectPersistence;
+import com.lhf.game.EffectPersistence.TickType;
+import com.lhf.game.creature.CreatureEffector.BasicCreatureEffector;
 import com.lhf.game.enums.EquipmentSlots;
 import com.lhf.game.enums.EquipmentTypes;
 import com.lhf.game.enums.Stats;
 import com.lhf.game.item.interfaces.Equipable;
 
 public class LeatherArmor extends Equipable {
-    private int AC = 2;
-
-    private List<EquipmentSlots> slots;
-    private List<EquipmentTypes> types;
-    private Map<String, Integer> equippingChanges;
+    private final int AC = 2;
 
     public LeatherArmor(boolean isVisible) {
         super("Leather Armor", isVisible);
 
-        slots = Collections.singletonList(EquipmentSlots.ARMOR);
-        types = Arrays.asList(EquipmentTypes.LIGHTARMOR, EquipmentTypes.LEATHER);
-        equippingChanges = new HashMap<>();
-        equippingChanges.put(Stats.AC.toString(), this.AC);
-    }
-
-    @Override
-    public List<EquipmentTypes> getTypes() {
-        return types;
-    }
-
-    @Override
-    public List<EquipmentSlots> getWhichSlots() {
-        return slots;
-    }
-
-    @Override
-    public Map<String, Integer> getEquippingChanges() {
-        return this.equippingChanges;
+        this.slots = Collections.singletonList(EquipmentSlots.ARMOR);
+        this.types = List.of(EquipmentTypes.LIGHTARMOR, EquipmentTypes.LEATHER);
+        this.equipEffects = Collections.singletonList(
+                new BasicCreatureEffector(null, this, new EffectPersistence(TickType.CONDITIONAL))
+                        .addStatChange(Stats.AC, this.AC));
     }
 
     @Override
     public String printDescription() {
-        return "This is some simple leather armor. " + "There is only a little blood on it...\n" +
-                this.printStats();
+        return "This is some simple leather armor. " + "There is only a little blood on it...\n";
     }
 }
