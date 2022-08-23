41a0447e85b316b079bff1edd48cac7257a2b6e1
Drake Foltz <GeenDutchman@byu.net>
2022-08-23 00:04:19 -0600

    another enum

--- gameserver/src/main/java/com/lhf/game/dice/DieType.java
+++ gameserver/src/main/java/com/lhf/game/dice/DieType.java
@@ -3,6 +3,19 @@ package com.lhf.game.dice;
 public enum DieType {
     HUNDRED(100), TWENTY(20), TWELVE(12), TEN(10), EIGHT(8), SIX(6), FOUR(4), TWO(2), NONE(0); // coin
 
+    public static DieType getDieType(String value) {
+        for (DieType dType : values()) {
+            if (dType.toString().equalsIgnoreCase(value)) {
+                return dType;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isDieType(String value) {
+        return DieType.getDieType(value) != null;
+    }
+
     private int type;
 
     DieType(int type) {
