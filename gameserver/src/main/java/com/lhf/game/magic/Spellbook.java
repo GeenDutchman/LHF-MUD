526359af1c5f05b0b13c1aa51a076acfd47b916c
Drake Foltz <GeenDutchman@byu.net>
2022-08-25 22:04:02 -0600

    pulled out spellbook

---
+++ gameserver/src/main/java/com/lhf/game/magic/Spellbook.java
@@ -0,0 +1,118 @@
+package com.lhf.game.magic;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
+import com.lhf.game.EntityEffectSource;
+import com.lhf.game.creature.CreatureEffectSource;
+import com.lhf.game.magic.concrete.ShockBolt;
+import com.lhf.game.magic.concrete.Thaumaturgy;
+import com.lhf.game.magic.concrete.ThunderStrike;
+import com.lhf.game.map.DungeonEffectSource;
+import com.lhf.game.map.RoomEffectSource;
+
+public class Spellbook {
+    private SortedSet<SpellEntry> entries;
+    private String path;
+    private final String[] path_to_spellbook = { ".", "concrete" };
+
+    public Spellbook() {
+        this.entries = new TreeSet<>();
+        SpellEntry shockBolt = new ShockBolt();
+        this.entries.add(shockBolt);
+        SpellEntry thaumaturgy = new Thaumaturgy();
+        this.entries.add(thaumaturgy);
+        SpellEntry thunderStrike = new ThunderStrike();
+        this.entries.add(thunderStrike);
+        this.setupPath();
+    }
+
+    private void setupPath() {
+        StringBuilder makePath = new StringBuilder();
+        for (String part : path_to_spellbook) {
+            makePath.append(part).append(File.separator);
+        }
+        this.path = getClass().getResource(makePath.toString()).getPath().replaceAll("target(.)classes",
+                "src$1main$1resources");
+    }
+
+    private Gson getAdaptedGson() {
+        RuntimeTypeAdapterFactory<SpellEntry> spellEntryAdapter = RuntimeTypeAdapterFactory
+                .of(SpellEntry.class, "className")
+                .registerSubtype(CreatureTargetingSpellEntry.class, CreatureTargetingSpellEntry.class.getName())
+                .registerSubtype(CreatureAOESpellEntry.class, CreatureAOESpellEntry.class.getName())
+                .registerSubtype(RoomTargetingSpellEntry.class, RoomTargetingSpellEntry.class.getName())
+                .registerSubtype(DungeonTargetingSpellEntry.class, DungeonTargetingSpellEntry.class.getName())
+                .registerSubtype(ShockBolt.class, ShockBolt.class.getName())
+                .registerSubtype(ThunderStrike.class, ThunderStrike.class.getName())
+                .registerSubtype(Thaumaturgy.class, Thaumaturgy.class.getName());
+        RuntimeTypeAdapterFactory<EntityEffectSource> effectSourceAdapter = RuntimeTypeAdapterFactory
+                .of(EntityEffectSource.class, "className")
+                .registerSubtype(CreatureEffectSource.class, CreatureEffectSource.class.getName())
+                .registerSubtype(RoomEffectSource.class, RoomEffectSource.class.getName())
+                .registerSubtype(DungeonEffectSource.class, DungeonEffectSource.class.getName());
+        GsonBuilder gb = new GsonBuilder().registerTypeAdapterFactory(spellEntryAdapter)
+                .registerTypeAdapterFactory(effectSourceAdapter).setPrettyPrinting();
+        return gb.create();
+    }
+
+    public boolean saveToFile() throws IOException {
+        return this.saveToFile(true);
+    }
+
+    @Deprecated(forRemoval = false)
+    private boolean saveToFile(boolean loadFirst) throws IOException {
+        if (loadFirst && !this.loadFromFile()) {
+            throw new IOException("Cannot preload spellbook!");
+        }
+        Gson gson = this.getAdaptedGson();
+        System.out.println("Writing to " + this.path);
+        try (FileWriter fileWriter = new FileWriter(this.path + "spellbook.json")) {
+            String asJson = gson.toJson(this.entries);
+            System.out.println(asJson);
+            fileWriter.write(asJson);
+        } catch (JsonIOException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean loadFromFile() {
+        Gson gson = this.getAdaptedGson();
+        System.out.println("Reading from " + this.path + "spellbook.json");
+        Integer preSize = this.entries.size();
+        try (JsonReader jReader = new JsonReader(new FileReader(this.path + "spellbook.json"))) {
+            Type collectionType = new TypeToken<TreeSet<SpellEntry>>() {
+            }.getType();
+            SortedSet<SpellEntry> retrieved = gson.fromJson(jReader, collectionType);
+            System.out.println(retrieved);
+            this.entries.addAll(retrieved);
+        } catch (JsonIOException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        System.out.printf("Spellbook size changed by %d\n", this.entries.size() - preSize);
+        return true;
+    }
+
+    public SortedSet<SpellEntry> getEntries() {
+        return Collections.unmodifiableSortedSet(entries);
+    }
+
+    public boolean addEntry(SpellEntry entry) {
+        return this.entries.add(entry);
+    }
+}
--- gameserver/src/main/java/com/lhf/game/magic/ThirdPower.java
+++ gameserver/src/main/java/com/lhf/game/magic/ThirdPower.java
@@ -1,35 +1,17 @@
 package com.lhf.game.magic;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonIOException;
-import com.google.gson.reflect.TypeToken;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
 import com.lhf.game.EffectResistance;
-import com.lhf.game.EntityEffectSource;
 import com.lhf.game.battle.BattleManager;
 import com.lhf.game.creature.Creature;
 import com.lhf.game.creature.CreatureEffect;
-import com.lhf.game.creature.CreatureEffectSource;
 import com.lhf.game.creature.vocation.Vocation.VocationName;
 import com.lhf.game.dice.MultiRollResult;
 import com.lhf.game.enums.CreatureFaction;
 import com.lhf.game.magic.CreatureAOESpellEntry.AutoSafe;
-import com.lhf.game.magic.concrete.ShockBolt;
-import com.lhf.game.magic.concrete.Thaumaturgy;
-import com.lhf.game.magic.concrete.ThunderStrike;
-import com.lhf.game.map.DungeonEffectSource;
-import com.lhf.game.map.RoomEffectSource;
 import com.lhf.messages.ClientMessenger;
 import com.lhf.messages.Command;
 import com.lhf.messages.CommandContext;
@@ -58,92 +40,19 @@ public class ThirdPower implements MessageHandler {
      * 
      * 
      */
-    private String path;
-    private SortedSet<SpellEntry> entries;
     private MessageHandler successor;
     private HashMap<CommandMessage, String> cmds;
-    private final String[] path_to_spellbook = { ".", "concrete" };
+    private Spellbook spellbook;
 
-    public ThirdPower(MessageHandler successor) {
+    public ThirdPower(MessageHandler successor, Spellbook spellbook) {
         this.successor = successor;
-        this.entries = new TreeSet<>();
-        SpellEntry shockBolt = new ShockBolt();
-        this.entries.add(shockBolt);
-        SpellEntry thaumaturgy = new Thaumaturgy();
-        this.entries.add(thaumaturgy);
-        SpellEntry thunderStrike = new ThunderStrike();
-        this.entries.add(thunderStrike);
         this.cmds = this.generateCommands();
-        this.setupPath();
-    }
-
-    private void setupPath() {
-        StringBuilder makePath = new StringBuilder();
-        for (String part : path_to_spellbook) {
-            makePath.append(part).append(File.separator);
+        if (spellbook != null) {
+            this.spellbook = new Spellbook();
+            this.spellbook.loadFromFile();
+        } else {
+            this.spellbook = spellbook;
         }
-        this.path = getClass().getResource(makePath.toString()).getPath().replaceAll("target(.)classes",
-                "src$1main$1resources");
-    }
-
-    private Gson getAdaptedGson() {
-        RuntimeTypeAdapterFactory<SpellEntry> spellEntryAdapter = RuntimeTypeAdapterFactory
-                .of(SpellEntry.class, "className")
-                .registerSubtype(CreatureTargetingSpellEntry.class, CreatureTargetingSpellEntry.class.getName())
-                .registerSubtype(CreatureAOESpellEntry.class, CreatureAOESpellEntry.class.getName())
-                .registerSubtype(RoomTargetingSpellEntry.class, RoomTargetingSpellEntry.class.getName())
-                .registerSubtype(DungeonTargetingSpellEntry.class, DungeonTargetingSpellEntry.class.getName())
-                .registerSubtype(ShockBolt.class, ShockBolt.class.getName())
-                .registerSubtype(ThunderStrike.class, ThunderStrike.class.getName())
-                .registerSubtype(Thaumaturgy.class, Thaumaturgy.class.getName());
-        RuntimeTypeAdapterFactory<EntityEffectSource> effectSourceAdapter = RuntimeTypeAdapterFactory
-                .of(EntityEffectSource.class, "className")
-                .registerSubtype(CreatureEffectSource.class, CreatureEffectSource.class.getName())
-                .registerSubtype(RoomEffectSource.class, RoomEffectSource.class.getName())
-                .registerSubtype(DungeonEffectSource.class, DungeonEffectSource.class.getName());
-        GsonBuilder gb = new GsonBuilder().registerTypeAdapterFactory(spellEntryAdapter)
-                .registerTypeAdapterFactory(effectSourceAdapter).setPrettyPrinting();
-        return gb.create();
-    }
-
-    public boolean saveToFile() throws IOException {
-        return this.saveToFile(true);
-    }
-
-    @Deprecated(forRemoval = false)
-    private boolean saveToFile(boolean loadFirst) throws IOException {
-        if (loadFirst && !this.loadFromFile()) {
-            throw new IOException("Cannot preload spellbook!");
-        }
-        Gson gson = this.getAdaptedGson();
-        System.out.println("Writing to " + this.path);
-        try (FileWriter fileWriter = new FileWriter(this.path + "spellbook.json")) {
-            String asJson = gson.toJson(this.entries);
-            System.out.println(asJson);
-            fileWriter.write(asJson);
-        } catch (JsonIOException | IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    public boolean loadFromFile() {
-        Gson gson = this.getAdaptedGson();
-        System.out.println("Reading from " + this.path + "spellbook.json");
-        Integer preSize = this.entries.size();
-        try (JsonReader jReader = new JsonReader(new FileReader(this.path + "spellbook.json"))) {
-            Type collectionType = new TypeToken<TreeSet<SpellEntry>>() {
-            }.getType();
-            SortedSet<SpellEntry> retrieved = gson.fromJson(jReader, collectionType);
-            System.out.println(retrieved);
-            this.entries.addAll(retrieved);
-        } catch (JsonIOException | IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        System.out.printf("Spellbook size changed by %d\n", this.entries.size() - preSize);
-        return true;
     }
 
     private HashMap<CommandMessage, String> generateCommands() {
@@ -160,13 +69,13 @@ public class ThirdPower implements MessageHandler {
 
     public SortedSet<SpellEntry> filterByExactLevel(int level) {
         Supplier<TreeSet<SpellEntry>> sortSupplier = () -> new TreeSet<SpellEntry>();
-        return this.entries.stream().filter(entry -> entry.getLevel() == level)
+        return this.spellbook.getEntries().stream().filter(entry -> entry.getLevel() == level)
                 .collect(Collectors.toCollection(sortSupplier));
     }
 
     public SortedSet<SpellEntry> filterByVocationName(VocationName vocationName) {
         Supplier<TreeSet<SpellEntry>> sortSupplier = () -> new TreeSet<SpellEntry>();
-        return this.entries.stream().filter(
+        return this.spellbook.getEntries().stream().filter(
                 entry -> entry.getAllowedVocations().size() == 0 ||
                         (vocationName != null && entry.getAllowedVocations().contains(vocationName)) ||
                         VocationName.DUNGEON_MASTER.equals(vocationName))
@@ -175,7 +84,7 @@ public class ThirdPower implements MessageHandler {
 
     public SortedSet<SpellEntry> filterByVocationAndLevels(VocationName vocationName, Collection<Integer> levels) {
         Supplier<TreeSet<SpellEntry>> sortSupplier = () -> new TreeSet<SpellEntry>();
-        return this.entries.stream().filter(
+        return this.spellbook.getEntries().stream().filter(
                 entry -> entry.getAllowedVocations().size() == 0 ||
                         (vocationName != null && entry.getAllowedVocations().contains(vocationName)) ||
                         VocationName.DUNGEON_MASTER.equals(vocationName))
@@ -184,11 +93,12 @@ public class ThirdPower implements MessageHandler {
     }
 
     public Optional<SpellEntry> filterByExactName(String name) {
-        return this.entries.stream().filter(entry -> entry.getName().equals(name)).findFirst();
+        return this.spellbook.getEntries().stream().filter(entry -> entry.getName().equals(name)).findFirst();
     }
 
     public Optional<SpellEntry> filterByExactInvocation(String invocation) {
-        return this.entries.stream().filter(entry -> entry.getInvocation().equals(invocation)).findFirst();
+        return this.spellbook.getEntries().stream().filter(entry -> entry.getInvocation().equals(invocation))
+                .findFirst();
     }
 
     private boolean affectCreatures(CommandContext ctx, ISpell<CreatureEffect> spell, Collection<Creature> targets) {
