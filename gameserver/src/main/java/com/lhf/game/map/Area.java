7581305d8beaaaa807c30b698cd85efbfe03978c
Drake Foltz <GeenDutchman@byu.net>
2023-02-10 22:20:39 -0700

    more interfaces

---
+++ gameserver/src/main/java/com/lhf/game/map/Area.java
@@ -0,0 +1,103 @@
+package com.lhf.game.map;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import com.lhf.game.AffectableEntity;
+import com.lhf.game.CreatureContainerMessageHandler;
+import com.lhf.game.ItemContainer;
+import com.lhf.game.creature.Creature;
+import com.lhf.game.creature.Monster;
+import com.lhf.game.creature.NonPlayerCharacter;
+import com.lhf.game.creature.Player;
+import com.lhf.game.item.Item;
+import com.lhf.game.item.Takeable;
+import com.lhf.messages.MessageHandler;
+import com.lhf.messages.out.SeeOutMessage;
+import com.lhf.messages.out.SeeOutMessage.SeeCategory;
+
+public interface Area
+        extends ItemContainer, CreatureContainerMessageHandler, Comparable<Area>, AffectableEntity<RoomEffect> {
+
+    public interface AreaBuilder {
+        public abstract String getName();
+
+        public abstract String getDescription();
+
+        public abstract Land getLand();
+
+        public abstract Collection<Item> getItems();
+
+        public abstract Collection<Creature> getCreatures();
+
+        public abstract MessageHandler getSuccessor();
+
+        public abstract Area build();
+    }
+
+    public abstract UUID getUuid();
+
+    public abstract Creature removeCreature(Creature c, Directions dir);
+
+    public abstract Land getLand();
+
+    @Override
+    default SeeOutMessage produceMessage() {
+        return this.produceMessage(false, true);
+    }
+
+    default SeeOutMessage produceMessage(boolean seeInvisible, boolean seeDirections) {
+        SeeOutMessage seen = new SeeOutMessage(this);
+        if (seeDirections) {
+            if (this.getLand() == null) {
+                seen.addExtraInfo("There is no apparent way out of here.");
+            } else {
+                Land land = this.getLand();
+                Map<Directions, Doorway> exits = land.getAreaExits(this);
+                if (exits != null) {
+                    for (Directions dir : exits.keySet()) {
+                        seen.addSeen(SeeCategory.DIRECTION, dir);
+                    }
+                }
+            }
+        }
+        for (Creature c : this.getCreatures()) {
+            if (c instanceof Player) {
+                seen.addSeen(SeeCategory.PLAYER, c);
+            } else if (c instanceof Monster) {
+                seen.addSeen(SeeCategory.MONSTER, c);
+            } else if (c instanceof NonPlayerCharacter) {
+                seen.addSeen(SeeCategory.NPC, c);
+            } else {
+                seen.addSeen(SeeCategory.CREATURE, c);
+            }
+        }
+        for (Item item : this.getItems()) {
+            if (!item.checkVisibility() && !seeInvisible) {
+                continue;
+            }
+            if (item instanceof Takeable) {
+                seen.addSeen(item.checkVisibility() ? SeeCategory.TAKEABLE : SeeCategory.INVISIBLE_TAKEABLE,
+                        item);
+            } else {
+                seen.addSeen(item.checkVisibility() ? SeeCategory.ROOM_ITEM : SeeCategory.INVISIBLE_ROOM_ITEM,
+                        item);
+            }
+        }
+        return seen;
+    }
+
+    @Override
+    public default int compareTo(Area o) {
+        if (this.equals(o)) {
+            return 0;
+        }
+        int nameCompare = this.getName().compareTo(o.getName());
+        if (nameCompare != 0) {
+            return nameCompare;
+        }
+        return this.getUuid().compareTo(o.getUuid());
+    }
+}
---
+++ gameserver/src/main/java/com/lhf/game/map/Land.java
@@ -0,0 +1,101 @@
+package com.lhf.game.map;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
+import com.lhf.game.AffectableEntity;
+import com.lhf.game.CreatureContainer;
+import com.lhf.game.creature.Creature;
+import com.lhf.game.creature.Player;
+import com.lhf.messages.MessageHandler;
+import com.lhf.server.client.user.UserID;
+
+public interface Land extends CreatureContainer, MessageHandler, AffectableEntity<DungeonEffect> {
+    public interface LandBuilder {
+        public abstract Area getStartingArea();
+
+        public abstract Map<UUID, AreaDirectionalLinks> getAtlas();
+
+        public abstract MessageHandler getSuccessor();
+
+        public abstract Land build();
+    }
+
+    public interface AreaDirectionalLinks {
+        public abstract Area getArea();
+
+        public abstract Map<Directions, Doorway> getExits();
+
+    }
+
+    public abstract Map<UUID, AreaDirectionalLinks> getAtlas();
+
+    public abstract Area getStartingArea();
+
+    public default Map<Directions, Doorway> getAreaExits(Area area) {
+        Map<UUID, AreaDirectionalLinks> atlas = this.getAtlas();
+        if (atlas == null || atlas.size() == 0 || !atlas.containsKey(area.getUuid())) {
+            return null;
+        }
+        return atlas.get(area.getUuid()).getExits();
+    }
+
+    public default Area getCreatureArea(Creature creature) {
+        for (AreaDirectionalLinks adl : this.getAtlas().values()) {
+            Area adlArea = adl.getArea();
+            if (adlArea != null) {
+                if (adlArea.hasCreature(creature)) {
+                    return adlArea;
+                }
+            }
+        }
+        return null;
+    }
+
+    public default Area getCreatureArea(String name) {
+        for (AreaDirectionalLinks adl : this.getAtlas().values()) {
+            Area adlArea = adl.getArea();
+            if (adlArea != null) {
+                if (adlArea.hasCreature(name, null)) {
+                    return adlArea;
+                }
+            }
+        }
+        return null;
+    }
+
+    public default Area getPlayerArea(UserID id) {
+        for (AreaDirectionalLinks rAndD : this.getAtlas().values()) {
+            Area randArea = rAndD.getArea();
+            if (randArea != null) {
+                Optional<Player> found = randArea.getPlayer(id);
+                if (found.isPresent()) {
+                    return randArea;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public default Collection<Creature> getCreatures() {
+        Set<Creature> creatures = new TreeSet<>();
+        Area startingArea = this.getStartingArea();
+        if (startingArea != null) {
+            creatures.addAll(startingArea.getCreatures());
+        }
+        for (AreaDirectionalLinks rAndD : this.getAtlas().values()) {
+            Area area = rAndD.getArea();
+            if (area != null) {
+                creatures.addAll(area.getCreatures());
+            }
+        }
+        return Collections.unmodifiableSet(creatures);
+    }
+
+}
