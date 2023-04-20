10bc0b1b03b5ba7095958e57e3f5b454712e7626
Drake Foltz <GeenDutchman@byu.net>
2023-04-20 12:05:58 -0600

    made vocation chooser

---
+++ gameserver/src/main/java/com/lhf/game/creature/intelligence/choosers/VocationChooser.java
@@ -0,0 +1,50 @@
+package com.lhf.game.creature.intelligence.choosers;
+
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.lhf.game.creature.intelligence.BasicAI.BattleMemories;
+import com.lhf.game.creature.intelligence.BasicAI.BattleMemories.BattleStats;
+import com.lhf.game.creature.vocation.Vocation.VocationName;
+import com.lhf.game.enums.CreatureFaction;
+
+public class VocationChooser implements TargetChooser {
+
+    private final List<VocationName> targetOrder;
+
+    public VocationChooser() {
+        this.targetOrder = List.of(VocationName.HEALER, VocationName.MAGE, VocationName.FIGHTER);
+    }
+
+    public VocationChooser(List<VocationName> vocationOrder) {
+        if (vocationOrder != null) {
+            this.targetOrder = List.copyOf(vocationOrder);
+        } else {
+            this.targetOrder = List.of(VocationName.HEALER, VocationName.MAGE, VocationName.FIGHTER);
+        }
+    }
+
+    @Override
+    public SortedMap<String, Float> chooseTarget(BattleMemories battleMemories, CreatureFaction myFaction) {
+        SortedMap<String, Float> results = new TreeMap<>();
+        Map<String, BattleStats> stats = battleMemories.getBattleStats();
+        if (stats != null) {
+            for (BattleStats stat : stats.values()) {
+                float priority = 0.1f;
+                for (int i = 0; i < this.targetOrder.size(); i++) {
+                    VocationName name = this.targetOrder.get(i);
+                    if (name != null && stat.getVocation() != null
+                            && name.equals(stat.getVocation().getVocationName())) {
+                        priority = (this.targetOrder.size() - i) / (float) this.targetOrder.size();
+                        break;
+                    }
+                }
+                results.put(stat.getTargetName(), priority);
+            }
+        }
+        return results;
+    }
+
+}
