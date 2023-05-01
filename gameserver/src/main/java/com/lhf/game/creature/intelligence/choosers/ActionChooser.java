821e0a4de9aab5f524dfbbeb085508fc8bf231fd
Drake Foltz <foltz.dw@gmail.com>
2023-04-30 20:10:54 -0600

    Make it comparable, in case we want to map it

--- gameserver/src/main/java/com/lhf/game/creature/intelligence/choosers/ActionChooser.java
+++ gameserver/src/main/java/com/lhf/game/creature/intelligence/choosers/ActionChooser.java
@@ -5,6 +5,15 @@ import java.util.SortedMap;
 import com.lhf.game.creature.intelligence.BasicAI.BattleMemories;
 import com.lhf.game.enums.CreatureFaction;
 
-public interface ActionChooser {
+public interface ActionChooser extends Comparable<ActionChooser> {
     public SortedMap<String, Float> chooseTarget(BattleMemories battleMemories, CreatureFaction myFaction);
+
+    @Override
+    public default int compareTo(ActionChooser arg0) {
+        if (arg0 == null) {
+            return 1;
+        }
+        return this.getClass().getName().compareTo(arg0.getClass().getName());
+    }
+
 }
