a116a64723e9d4eef5ef5e222387ce34f4d0b189
Drake Foltz <foltz.dw@gmail.com>
2023-10-10 22:32:51 -0600

    simplify spell chooser

--- gameserver/src/main/java/com/lhf/game/creature/intelligence/actionChoosers/SpellChooser.java
+++ gameserver/src/main/java/com/lhf/game/creature/intelligence/actionChoosers/SpellChooser.java
@@ -1,71 +1,37 @@
 package com.lhf.game.creature.intelligence.actionChoosers;
 
 import java.util.Collection;
-import java.util.DoubleSummaryStatistics;
 import java.util.Map;
-import java.util.NavigableSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import com.lhf.game.creature.NonPlayerCharacter.HarmMemories;
 import com.lhf.game.creature.intelligence.AIChooser;
 import com.lhf.game.enums.CreatureFaction;
-import com.lhf.game.enums.HealthBuckets;
 import com.lhf.game.magic.SpellEntry;
 import com.lhf.messages.out.OutMessage;
 import com.lhf.messages.out.SpellEntryMessage;
 import com.lhf.messages.out.StatsOutMessage;
 
 public class SpellChooser implements AIChooser<SpellEntry> {
-    private final boolean offensiveFocus;
+    private final double offensiveFocus;
 
-    public SpellChooser(boolean offensiveFocus) {
-        this.offensiveFocus = offensiveFocus;
+    public SpellChooser(double offensiveFocus) {
+        double temp = Math.max(AIChooser.MIN_VALUE, offensiveFocus);
+        this.offensiveFocus = Math.min(1 - AIChooser.MIN_VALUE, temp);
     }
 
-    private HealthBuckets factionHealth(StatsOutMessage battleMemories, Set<CreatureFaction> targetFactions) {
-        if (targetFactions == null || targetFactions.isEmpty() || battleMemories == null) {
-            return null;
-        }
-        DoubleSummaryStatistics sum = battleMemories.getRecords().stream()
-                .filter(statRecord -> statRecord != null && statRecord.getBucket() != null
-                        && targetFactions.contains(statRecord.getFaction()))
-                .map(statRecord -> statRecord.getBucket())
-                .collect(Collectors.summarizingDouble(HealthBuckets::getValue));
-
-        if (sum.getCount() < 1) {
-            return null;
-        }
-        HealthBuckets avgBucket = HealthBuckets.fromPercent(sum.getAverage());
-        return avgBucket;
-    }
-
-    private Map<SpellEntry, Double> offensiveChoice(Optional<StatsOutMessage> battleMemories,
+    private Map<SpellEntry, Double> offensiveBalanceSelection(Optional<StatsOutMessage> battleMemories,
             HarmMemories harmMemories,
             Set<CreatureFaction> targetFactions, SpellEntryMessage entries) {
         SortedMap<SpellEntry, Double> selection = entries.getEntries().stream()
-                .filter(entry -> entry != null && entry.isOffensive())
+                .filter(entry -> entry != null)
                 .collect(Collectors.toMap(spellentry -> spellentry,
-                        spellentry -> (double) (spellentry.aiScore() / (spellentry.getLevel().toInt() + 0.1)),
-                        (scorea, scoreb) -> (scorea + scoreb) / 2, TreeMap::new));
-        if (selection.isEmpty()) {
-            return selection;
-        }
-
-        return selection;
-    }
-
-    private Map<SpellEntry, Double> notOffensiveChoice(Optional<StatsOutMessage> battleMemories,
-            HarmMemories harmMemories,
-            Set<CreatureFaction> targetFactions, SpellEntryMessage entries) {
-        SortedMap<SpellEntry, Double> selection = entries.getEntries().stream()
-                .filter(entry -> entry != null && !entry.isOffensive())
-                .collect(Collectors.toMap(spellentry -> spellentry,
-                        spellentry -> (double) (spellentry.aiScore() / (spellentry.getLevel().toInt() + 0.1)),
+                        spellentry -> (spellentry.aiScore() / (spellentry.getLevel().toInt() + 0.1))
+                                * (spellentry.isOffensive() ? this.offensiveFocus : 1 - this.offensiveFocus),
                         (scorea, scoreb) -> (scorea + scoreb) / 2, TreeMap::new));
         if (selection.isEmpty()) {
             return selection;
@@ -88,8 +54,7 @@ public class SpellChooser implements AIChooser<SpellEntry> {
         if (sem.isEmpty() || sem.get().getEntries().isEmpty()) {
             return selection;
         }
-        selection.putAll(this.notOffensiveChoice(battleMemories, harmMemories, targetFactions, sem.get()));
-        selection.putAll(this.offensiveChoice(battleMemories, harmMemories, targetFactions, sem.get()));
+        selection.putAll(this.offensiveBalanceSelection(battleMemories, harmMemories, targetFactions, sem.get()));
         return selection;
     }
 
