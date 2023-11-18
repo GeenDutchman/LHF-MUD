1ed1dd4efbc09bb8219dbf917fd66115c5b1ff04
Drake Foltz <foltz.dw@gmail.com>
2023-11-17 17:10:47 -0700

    Abilities to be taken by Vocation for now

--- gameserver/src/main/java/com/lhf/game/creature/statblock/Statblock.java
+++ gameserver/src/main/java/com/lhf/game/creature/statblock/Statblock.java
@@ -73,10 +73,6 @@ public class Statblock {
      * // and example item would be lightCrossbow
      */
     private Set<EquipmentTypes> proficiencies;
-    /*
-     * TODO: or to add once things are done
-     * Abilities... initial thought was some kind of array
-     */
 
     // Inventory
     private Inventory inventory;
