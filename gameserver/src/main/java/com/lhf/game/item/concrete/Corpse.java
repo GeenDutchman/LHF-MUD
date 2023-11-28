8b962cd0385136699a97ea5e1d92f1d31e7cbc4e
Drake Foltz <foltz.dw@gmail.com>
2023-11-28 14:25:13 -0700

    Corpses don't lock silly!

--- gameserver/src/main/java/com/lhf/game/item/concrete/Corpse.java
+++ gameserver/src/main/java/com/lhf/game/item/concrete/Corpse.java
@@ -1,5 +1,7 @@
 package com.lhf.game.item.concrete;
 
+import com.lhf.game.creature.inventory.InventoryOwner;
+
 public class Corpse extends Chest {
     public Corpse(String name, boolean isVisible) {
         super(name, isVisible);
@@ -9,6 +11,16 @@ public class Corpse extends Chest {
     public String printDescription() {
         return "This is " + this.getColorTaggedName()
                 + ".  They are quite clearly dead.  You can't quite tell the cause...";
+    }
+
+    @Override
+    public boolean isUnlocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isAuthorized(InventoryOwner attemtper) {
+        return true;
     }
 
 }
