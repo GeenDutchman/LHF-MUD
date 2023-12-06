d06ca290ee91ef6821da4f623676fbd06a6fe214
Drake Foltz <foltz.dw@gmail.com>
2023-12-05 22:50:28 -0700

    I have a good interface idea, documented

---
+++ gameserver/src/main/java/com/lhf/messages/PooledMessageChainHandler.java
@@ -0,0 +1,242 @@
+package com.lhf.messages;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+
+import com.lhf.messages.CommandContext.Reply;
+
+/**
+ * Meant to hold a buffer of {@link com.lhf.messages.Command Command}s for a
+ * time.
+ * These pooled commands remain until {@link #flush() flushed}.
+ * The pools are Keyed by something {@link java.lang.Comparable Comparable} to
+ * other Keys.
+ */
+public interface PooledMessageChainHandler<Key extends Comparable<Key>> extends MessageChainHandler {
+
+    /**
+     * General interface for a Pooled {@link com.lhf.messages.Command Command}
+     */
+    public interface IPoolEntry {
+
+        /**
+         * 
+         * @return the {@link com.lhf.messages.CommandContext Context} related to the
+         *         {@link com.lhf.messages.Command Command}
+         */
+        public CommandContext getContext();
+
+        /**
+         * 
+         * @return the pooled {@link com.lhf.messages.Command Command} itself
+         */
+        public Command getCommand();
+    }
+
+    /**
+     * Default concretion of a
+     * {@link com.lhf.messages.PooledMessageChainHandler.IPoolEntry IPoolEntry}.
+     * 
+     * Contains a {@link com.lhf.messages.CommandContext Context} and
+     * {@link com.lhf.messages.Command Command}.
+     */
+    public class PoolEntry implements IPoolEntry {
+        private final CommandContext ctx;
+        private final Command command;
+
+        public PoolEntry(CommandContext ctx, Command command) {
+            this.ctx = ctx;
+            this.command = command;
+        }
+
+        @Override
+        public CommandContext getContext() {
+            return ctx;
+        }
+
+        @Override
+        public Command getCommand() {
+            return command;
+        }
+
+    }
+
+    /**
+     * Returns the pools for each Key.
+     * 
+     * @return map of pools
+     */
+    public NavigableMap<Key, Deque<IPoolEntry>> getPools();
+
+    /**
+     * Attempts to place entry into pool
+     * 
+     * @param key
+     * @param entry
+     * @return true if successfully emplaced, false otherwise
+     */
+    public boolean empool(Key key, IPoolEntry entry);
+
+    /**
+     * Constructs a {@link com.lhf.messages.PooledMessageChainHandler.PoolEntry
+     * PoolEntry} and attempts to place it into pool
+     * 
+     * @param key
+     * @param ctx
+     * @param cmd
+     * @return true if successfully emplaced, false otherwise
+     */
+    public default boolean empool(Key key, CommandContext ctx, Command cmd) {
+        if (key == null || cmd == null) {
+            this.log(Level.WARNING, "cannot empool with null key or command");
+            return false;
+        }
+        if (ctx == null) {
+            ctx = new CommandContext();
+        }
+        ctx = this.addSelfToContext(ctx);
+        return this.empool(key, new PoolEntry(ctx, cmd));
+    }
+
+    /**
+     * Constructs a {@link com.lhf.messages.PooledMessageChainHandler.PoolEntry
+     * PoolEntry} and attempts to place it into pool
+     * 
+     * @param ctx
+     * @param cmd
+     * @return true if successfully emplaced, false otherwise
+     */
+    public default boolean empool(CommandContext ctx, Command cmd) {
+        if (cmd == null) {
+            this.log(Level.WARNING, "cannot empool with null command");
+            return false;
+        }
+        if (ctx == null) {
+            ctx = new CommandContext();
+        }
+        ctx = this.addSelfToContext(ctx);
+        Key key = this.keyFromContext(ctx);
+        if (key == null) {
+            this.log(Level.WARNING, () -> String.format("key is null and cannot empool, generated from %s", ctx));
+            return false;
+        }
+        return this.empool(key, new PoolEntry(ctx, cmd));
+    }
+
+    /**
+     * Attempts to generate a Key from the {@link com.lhf.messages.CommandContext
+     * Context}
+     * 
+     * @param ctx
+     * @return a Key if successful, null if not
+     */
+    public Key keyFromContext(CommandContext ctx);
+
+    /**
+     * Check if the all the pools are ready to flush. Should have no side effects.
+     * 
+     * @return true if ready, false if not
+     */
+    public boolean isReadyToFlush();
+
+    /**
+     * Goes through all the pools and handles each Command.
+     */
+    public void flush();
+
+    /**
+     * Used in conjuction with the {@link com.lhf.messages.PooledMessageChainHandler
+     * PooledMessageChainHandler} to pool {@link com.lhf.messages.Command Command}s.
+     * If a PooledCommandHandler is in a normal
+     * {@link com.lhf.messages.MessageChainHandler MessageChainHandler} then it will
+     * do nothing special.
+     */
+    public interface PooledCommandHandler extends MessageChainHandler.CommandHandler {
+
+        /**
+         * Said predicate should check if the {@link com.lhf.messages.CommandContext
+         * Context} allows for pooling.
+         * 
+         * @return Predicate to check for if the command can be pooled.
+         */
+        public Predicate<CommandContext> getPoolingPredicate();
+
+        /**
+         * Retrieves {@link #getPoolingPredicate()} and tests it, if present.
+         * 
+         * @param ctx
+         * @return true when the predicate is retrieved and tests positive, false
+         *         otherwise
+         */
+        public default boolean isPoolingEnabled(CommandContext ctx) {
+            Predicate<CommandContext> predicate = this.getPoolingPredicate();
+            return predicate == null ? false : predicate.test(ctx);
+        }
+
+        /**
+         * Used as a hook for events to happen before pooling.
+         * 
+         * @param ctx
+         * @param cmd
+         * @return
+         */
+        public CommandContext.Reply prePoolHandle(CommandContext ctx, Command cmd);
+    }
+
+    private static CommandContext addHelps(Map<CommandMessage, CommandHandler> handlers, CommandContext ctx) {
+        if (ctx == null) {
+            ctx = new CommandContext();
+        }
+        if (handlers == null) {
+            return ctx;
+        }
+        for (CommandHandler handler : handlers.values()) {
+            if (handler != null && handler.isEnabled(ctx)) {
+                Optional<String> helpString = handler.getHelp(ctx);
+                if (helpString != null && helpString.isPresent()) {
+                    ctx.addHelp(handler.getHandleType(), helpString.get());
+                }
+            }
+        }
+        return ctx;
+    }
+
+    @Override
+    public default Reply handle(CommandContext ctx, Command cmd) {
+        if (ctx == null) {
+            ctx = new CommandContext();
+        }
+        ctx = this.addSelfToContext(ctx);
+        Map<CommandMessage, CommandHandler> handlers = this.getCommands(ctx);
+        ctx = PooledMessageChainHandler.addHelps(handlers, ctx);
+        if (cmd != null && handlers != null) {
+            CommandHandler handler = handlers.get(cmd.getType());
+            if (handler == null) {
+                this.log(Level.FINEST,
+                        () -> String.format("No CommandHandler for type %s at this level", cmd.getType()));
+            } else if (handler.isEnabled(ctx)) {
+                CommandContext.Reply reply = null;
+                if (handler instanceof PooledCommandHandler pooledCommandHandler
+                        && pooledCommandHandler.isPoolingEnabled(ctx)) {
+                    reply = pooledCommandHandler.prePoolHandle(ctx, cmd);
+                    if (reply != null && this.empool(ctx, cmd) && !reply.isHandled()) {
+                        reply = reply.resolve();
+                    }
+                } else {
+                    reply = handler.handle(ctx, cmd);
+                }
+                if (reply == null) {
+                    this.log(Level.SEVERE,
+                            () -> String.format("No reply for handler of type %s", handler.getHandleType()));
+                } else if (reply.isHandled()) {
+                    return reply;
+                }
+            }
+        }
+        return ctx.failhandle();
+    }
+}
