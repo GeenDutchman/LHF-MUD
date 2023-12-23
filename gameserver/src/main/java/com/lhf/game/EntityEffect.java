381d660072b33a37d6d42fc76c59533f78949c2c
Drake Foltz <foltz.dw@gmail.com>
2023-12-22 22:30:39 -0700

    EntityEffect as a ClientMessenger

--- gameserver/src/main/java/com/lhf/game/EntityEffect.java
+++ gameserver/src/main/java/com/lhf/game/EntityEffect.java
@@ -1,25 +1,41 @@
 package com.lhf.game;
 
 import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.lhf.Taggable;
 import com.lhf.TaggedExaminable;
 import com.lhf.game.EffectPersistence.Ticker;
 import com.lhf.game.creature.ICreature;
+import com.lhf.messages.ClientMessenger;
+import com.lhf.messages.ITickEvent;
+import com.lhf.messages.events.GameEvent;
 import com.lhf.messages.events.SeeEvent;
+import com.lhf.server.client.ClientID;
 
-public abstract class EntityEffect implements TaggedExaminable, Comparable<EntityEffect> {
+public abstract class EntityEffect implements TaggedExaminable, Comparable<EntityEffect>, ClientMessenger {
 
     protected final EntityEffectSource source;
+    private final ClientID clientID;
+    private final static Logger logger = Logger.getLogger(EntityEffect.class.getName());
     protected ICreature creatureResponsible;
     protected Taggable generatedBy;
     protected Ticker ticker;
 
     public EntityEffect(EntityEffectSource source, ICreature creatureResponsible, Taggable generatedBy) {
         this.source = source;
+        this.clientID = new ClientID();
         this.creatureResponsible = creatureResponsible;
         this.generatedBy = generatedBy;
         this.ticker = null;
+    }
+
+    @Override
+    public ClientID getClientID() {
+        return this.clientID;
     }
 
     public ICreature creatureResponsible() {
@@ -54,6 +70,18 @@ public abstract class EntityEffect implements TaggedExaminable, Comparable<Entit
     }
 
     @Override
+    public Consumer<GameEvent> getAcceptHook() {
+        return event -> {
+            if (event == null) {
+                return;
+            }
+            if (event instanceof ITickEvent tickEvent) {
+                this.tick(tickEvent.getTickType());
+            }
+        };
+    }
+
+    @Override
     public int compareTo(EntityEffect o) {
         if (this.equals(o)) {
             return 0;
@@ -62,7 +90,11 @@ public abstract class EntityEffect implements TaggedExaminable, Comparable<Entit
         if (namecompare != 0) {
             return namecompare;
         }
-        return this.getPersistence().compareTo(o.getPersistence());
+        int persistenceCompare = this.getPersistence().compareTo(o.getPersistence());
+        if (persistenceCompare != 0) {
+            return persistenceCompare;
+        }
+        return this.getClientID().compareTo(o.getClientID());
     }
 
     @Override
@@ -102,20 +134,33 @@ public abstract class EntityEffect implements TaggedExaminable, Comparable<Entit
     }
 
     @Override
+    public void log(Level logLevel, String logMessage) {
+        EntityEffect.logger.log(logLevel,
+                String.format("%s by %s: %s", this.getName(), this.creatureResponsible, logMessage));
+    }
+
+    @Override
+    public void log(Level logLevel, Supplier<String> logMessageSupplier) {
+        Supplier<String> actualSupplier = () -> {
+            return String.format("%s by %s: %s", this.getName(), this.creatureResponsible,
+                    logMessageSupplier != null ? logMessageSupplier.get() : "no message");
+        };
+        EntityEffect.logger.log(logLevel, actualSupplier);
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(source);
+        return Objects.hash(source, clientID);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this == obj)
             return true;
-        }
-        if (!(obj instanceof EntityEffect)) {
+        if (!(obj instanceof EntityEffect))
             return false;
-        }
         EntityEffect other = (EntityEffect) obj;
-        return Objects.equals(source, other.source);
+        return Objects.equals(source, other.source) && Objects.equals(clientID, other.clientID);
     }
 
 }
