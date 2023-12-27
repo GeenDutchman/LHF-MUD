00c4cb3869996145314c7cdd68523387ce655f83
Drake Foltz <foltz.dw@gmail.com>
2023-12-27 12:15:47 -0700

    just go straight to UUID

--- gameserver/src/main/java/com/lhf/game/map/DoorwayFactory.java
+++ gameserver/src/main/java/com/lhf/game/map/DoorwayFactory.java
@@ -1,24 +1,26 @@
 package com.lhf.game.map;
 
+import java.util.UUID;
+
 public class DoorwayFactory {
     public enum DoorwayType {
         STANDARD, ONE_WAY, CLOSEABLE, KEYED;
     }
 
-    public static Doorway createDoorway(DoorwayType type, Room toAdd, Directions toExistingRoom, Room existing) {
+    public static Doorway createDoorway(DoorwayType type, UUID toAdd, Directions toExistingRoom, UUID existing) {
         if (type == null) {
-            return new Doorway(existing.getUuid(), toExistingRoom, toAdd.getUuid());
+            return new Doorway(existing, toExistingRoom, toAdd);
         }
         switch (type) {
             case ONE_WAY:
-                return new OneWayDoorway(existing.getUuid(), toExistingRoom, toAdd.getUuid());
+                return new OneWayDoorway(existing, toExistingRoom, toAdd);
             case CLOSEABLE:
-                return new CloseableDoorway(existing.getUuid(), toExistingRoom, toAdd.getUuid());
+                return new CloseableDoorway(existing, toExistingRoom, toAdd);
             case KEYED:
-                return new KeyedDoorway(existing.getUuid(), toExistingRoom, toAdd.getUuid());
+                return new KeyedDoorway(existing, toExistingRoom, toAdd);
             case STANDARD:
             default:
-                return new Doorway(existing.getUuid(), toExistingRoom, toAdd.getUuid());
+                return new Doorway(existing, toExistingRoom, toAdd);
         }
     }
 }
