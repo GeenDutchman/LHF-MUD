60a484e248c4f478d1424c7cbed3bf5f599b0480
Drake Foltz <foltz.dw@gmail.com>
2023-12-27 09:37:33 -0700

    DungeonMaster follows new builder

--- gameserver/src/main/java/com/lhf/game/creature/DungeonMaster.java
+++ gameserver/src/main/java/com/lhf/game/creature/DungeonMaster.java
@@ -1,35 +1,44 @@
 package com.lhf.game.creature;
 
+import java.io.FileNotFoundException;
+import java.util.function.UnaryOperator;
+
 import com.lhf.game.creature.intelligence.AIRunner;
+import com.lhf.game.creature.statblock.StatblockManager;
 import com.lhf.game.creature.vocation.DMV;
+import com.lhf.messages.CommandChainHandler;
 
 public class DungeonMaster extends NonPlayerCharacter {
 
-    public static class DungeonMasterBuilder extends INonPlayerCharacter.AbstractNPCBuilder<DungeonMasterBuilder> {
-        private DungeonMasterBuilder(AIRunner aiRunner) {
-            super(aiRunner);
+    public static class DungeonMasterBuilder
+            extends INonPlayerCharacter.AbstractNPCBuilder<DungeonMasterBuilder, DungeonMaster> {
+        private DungeonMasterBuilder() {
+            super();
             this.setVocation(new DMV());
         }
 
-        protected DungeonMaster register(DungeonMaster npc) {
-            if (this.getAiRunner() != null) {
-                this.getAiRunner().register(npc, this.getAiHandlersAsArray());
+        public static DungeonMasterBuilder getInstance() {
+            return new DungeonMasterBuilder();
+        }
+
+        @Override
+        protected DungeonMaster preEnforcedRegistrationBuild(CommandChainHandler successor,
+                StatblockManager statblockManager, UnaryOperator<DungeonMasterBuilder> composedlazyLoaders)
+                throws FileNotFoundException {
+            if (statblockManager != null) {
+                this.loadStatblock(statblockManager);
             }
-            return npc;
-        }
-
-        public static DungeonMasterBuilder getInstance(AIRunner aiRunner) {
-            return new DungeonMasterBuilder(aiRunner);
-        }
-
-        @Override
-        public DungeonMaster build() {
-            return this.register(this.preEnforcedRegistrationBuild());
+            if (composedlazyLoaders != null) {
+                composedlazyLoaders.apply(this.getThis());
+            }
+            DungeonMaster created = new DungeonMaster(this);
+            created.setSuccessor(successor);
+            return created;
         }
 
         @Override
-        protected DungeonMaster preEnforcedRegistrationBuild() {
-            return new DungeonMaster(this);
+        public boolean equals(Object obj) {
+            return super.equals(obj) && obj instanceof DungeonMasterBuilder;
         }
     }
 
@@ -37,8 +46,8 @@ public class DungeonMaster extends NonPlayerCharacter {
         super(builder);
     }
 
-    public static DungeonMasterBuilder getDMBuilder(AIRunner aiRunner) {
-        return new DungeonMasterBuilder(aiRunner);
+    public static DungeonMasterBuilder getDMBuilder() {
+        return new DungeonMasterBuilder();
     }
 
 }
