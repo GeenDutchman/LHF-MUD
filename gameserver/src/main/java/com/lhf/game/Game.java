cf5918b7a999f22c7aaa6f67c3c8b156e65ede7d
Drake Foltz <foltz.dw@gmail.com>
2023-12-29 14:14:20 -0700

    made a game builder

--- gameserver/src/main/java/com/lhf/game/Game.java
+++ gameserver/src/main/java/com/lhf/game/Game.java
@@ -1,6 +1,7 @@
 package com.lhf.game;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumMap;
@@ -14,15 +15,17 @@ import java.util.logging.Logger;
 
 import com.lhf.game.creature.Player;
 import com.lhf.game.creature.conversation.ConversationManager;
+import com.lhf.game.creature.intelligence.AIRunner;
 import com.lhf.game.creature.intelligence.GroupAIRunner;
 import com.lhf.game.creature.statblock.StatblockManager;
 import com.lhf.game.creature.vocation.Vocation;
 import com.lhf.game.creature.vocation.VocationFactory;
 import com.lhf.game.magic.ThirdPower;
 import com.lhf.game.map.DMRoom;
-import com.lhf.game.map.Dungeon;
+import com.lhf.game.map.DMRoom.DMRoomBuilder;
+import com.lhf.game.map.Land;
+import com.lhf.game.map.Land.LandBuilder;
 import com.lhf.game.map.StandardDungeonProducer;
-import com.lhf.game.map.Dungeon.DungeonBuilder;
 import com.lhf.messages.Command;
 import com.lhf.messages.CommandChainHandler;
 import com.lhf.messages.CommandContext;
@@ -33,7 +36,6 @@ import com.lhf.messages.events.PlayersListedEvent;
 import com.lhf.server.client.user.User;
 import com.lhf.server.client.user.UserID;
 import com.lhf.server.client.user.UserManager;
-import com.lhf.server.interfaces.NotNull;
 import com.lhf.server.interfaces.ServerInterface;
 import com.lhf.server.interfaces.UserListener;
 
@@ -44,50 +46,153 @@ public class Game implements UserListener, CommandChainHandler {
 	private final Logger logger;
 	private final ThirdPower thirdPower;
 	private DMRoom controlRoom;
-	private final GroupAIRunner groupAiRunner;
+	private final AIRunner groupAiRunner;
 	private final ConversationManager conversationManager;
 	private final StatblockManager statblockManager;
 	private Map<CommandMessage, CommandHandler> commands;
 	private final GameEventProcessorID gameEventProcessorID;
 
-	public Game(ServerInterface server, UserManager userManager) throws FileNotFoundException {
-		this.gameEventProcessorID = new GameEventProcessorID();
-		this.logger = Logger.getLogger(this.getClass().getName());
-		this.groupAiRunner = new GroupAIRunner(true);
-		this.thirdPower = new ThirdPower(this, null);
-		this.conversationManager = new ConversationManager();
-		this.statblockManager = new StatblockManager();
-		Dungeon dungeon = StandardDungeonProducer.buildStaticDungeonBuilder(this.thirdPower, this.groupAiRunner, this,
-				this.conversationManager, this.statblockManager).build(server, this);
-		this.controlRoom = DMRoom.DMRoomBuilder.buildDefault(groupAiRunner, statblockManager, conversationManager);
-		this.controlRoom.addLand(dungeon);
-		this.controlRoom.setSuccessor(this.thirdPower);
-		this.successor = server;
-		this.server = server;
-		if (this.server != null) {
-			this.server.registerCallback(this);
+	public static class GameBuilder {
+		private ServerInterface server;
+		private ThirdPower thirdPower;
+		private AIRunner aiRunner;
+		private ConversationManager conversationManager;
+		private StatblockManager statblockManager;
+		private DMRoomBuilder dmRoomBuilder;
+		private ArrayList<Land.LandBuilder> additionalLands;
+
+		public GameBuilder() {
+			this.server = null;
+			this.thirdPower = null;
+			this.aiRunner = null;
+			this.conversationManager = null;
+			this.statblockManager = null;
+			this.dmRoomBuilder = null;
+			this.additionalLands = new ArrayList<>();
 		}
-		this.userManager = userManager;
-		this.logger.log(Level.INFO, "Created Game");
-		this.commands = new EnumMap<>(CommandMessage.class);
-		this.commands.put(CommandMessage.PLAYERS, new PlayersHandler());
+
+		public ServerInterface getServer() {
+			return server;
+		}
+
+		public ThirdPower getThirdPower() {
+			if (this.thirdPower == null) {
+				this.thirdPower = new ThirdPower(null, null);
+			}
+			return thirdPower;
+		}
+
+		public AIRunner getAiRunner() {
+			if (this.aiRunner == null) {
+				this.aiRunner = new GroupAIRunner(true);
+			}
+			return aiRunner;
+		}
+
+		public ConversationManager getConversationManager() {
+			if (this.conversationManager == null) {
+				this.conversationManager = new ConversationManager();
+			}
+			return conversationManager;
+		}
+
+		public StatblockManager getStatblockManager() {
+			if (this.statblockManager == null) {
+				this.statblockManager = new StatblockManager();
+			}
+			return statblockManager;
+		}
+
+		public DMRoomBuilder getDmRoomBuilder() {
+			return dmRoomBuilder;
+		}
+
+		public ArrayList<Land.LandBuilder> getAdditionalLands() {
+			return additionalLands;
+		}
+
+		public GameBuilder setDefaults() throws FileNotFoundException {
+			this.thirdPower = new ThirdPower(null, null);
+			this.aiRunner = new GroupAIRunner(true);
+			this.conversationManager = new ConversationManager();
+			this.statblockManager = new StatblockManager();
+			this.additionalLands.add(0, StandardDungeonProducer.buildStaticDungeonBuilder(statblockManager));
+			return this;
+		}
+
+		public GameBuilder setServer(ServerInterface server) {
+			this.server = server;
+			return this;
+		}
+
+		public GameBuilder setThirdPower(ThirdPower thirdPower) {
+			this.thirdPower = thirdPower;
+			return this;
+		}
+
+		public GameBuilder setAiRunner(AIRunner aiRunner) {
+			this.aiRunner = aiRunner;
+			return this;
+		}
+
+		public GameBuilder setConversationManager(ConversationManager conversationManager) {
+			this.conversationManager = conversationManager;
+			return this;
+		}
+
+		public GameBuilder setStatblockManager(StatblockManager statblockManager) {
+			this.statblockManager = statblockManager;
+			return this;
+		}
+
+		public GameBuilder setDmRoomBuilder(DMRoomBuilder dmRoomBuilder) {
+			this.dmRoomBuilder = dmRoomBuilder;
+			return this;
+		}
+
+		public GameBuilder setAdditionalLands(ArrayList<Land.LandBuilder> additionalLands) {
+			this.additionalLands = additionalLands;
+			return this;
+		}
+
+		public Game build(UserManager userManager) throws FileNotFoundException {
+			Game game = new Game(this, userManager);
+			game.setServer(server);
+			if (this.thirdPower != null) {
+				this.thirdPower.setSuccessor(game);
+			}
+			return game;
+		}
 	}
 
-	public Game(ServerInterface server, UserManager userManager, GroupAIRunner aiRunner,
-			@NotNull DungeonBuilder dungeonBuilder)
-			throws FileNotFoundException {
+	public Game(GameBuilder builder, UserManager userManager) throws FileNotFoundException {
 		this.gameEventProcessorID = new GameEventProcessorID();
 		this.logger = Logger.getLogger(this.getClass().getName());
-		this.groupAiRunner = aiRunner;
-		this.thirdPower = new ThirdPower(this, null);
-		this.conversationManager = new ConversationManager();
-		this.statblockManager = new StatblockManager();
-		this.controlRoom = DMRoom.DMRoomBuilder.buildDefault(groupAiRunner, statblockManager, conversationManager);
-		Dungeon dungeon = dungeonBuilder.build(this.controlRoom, groupAiRunner, statblockManager, conversationManager);
-		this.controlRoom.addLand(dungeon);
-		this.controlRoom.setSuccessor(this.thirdPower);
-		this.successor = server;
-		this.server = server;
+		this.groupAiRunner = builder.getAiRunner();
+		this.thirdPower = builder.getThirdPower();
+		this.thirdPower.setSuccessor(this);
+		this.conversationManager = builder.getConversationManager();
+		this.statblockManager = builder.getStatblockManager();
+		DMRoom.DMRoomBuilder dmRoomBuilder = builder.getDmRoomBuilder();
+		if (dmRoomBuilder != null) {
+			this.controlRoom = dmRoomBuilder.build(this.thirdPower, null, groupAiRunner, statblockManager,
+					conversationManager);
+		} else {
+			this.controlRoom = DMRoom.DMRoomBuilder.buildDefault(groupAiRunner, statblockManager, conversationManager);
+			this.controlRoom.setSuccessor(this.thirdPower);
+		}
+		ArrayList<LandBuilder> moreLands = builder.getAdditionalLands();
+		if (moreLands != null) {
+			for (LandBuilder landBuilder : moreLands) {
+				if (landBuilder == null) {
+					continue;
+				}
+				this.controlRoom.addLand(
+						landBuilder.build(this.thirdPower, groupAiRunner, statblockManager, conversationManager));
+			}
+		}
+		this.successor = builder.getServer();
+		this.server = builder.getServer();
 		if (this.server != null) {
 			this.server.registerCallback(this);
 		}
@@ -100,7 +205,6 @@ public class Game implements UserListener, CommandChainHandler {
 	@Override
 	public void userConnected(UserID id) {
 		this.logger.entering(this.getClass().getName(), "userConnected()", id);
-		// this.dungeon.sendMessageToAllExcept(new NewInMessage(), id);
 	}
 
 	@Override
@@ -211,7 +315,9 @@ public class Game implements UserListener, CommandChainHandler {
 	}
 
 	public void setServer(ServerInterface server) {
-		// TODO: unregister from old server?
+		if (this.server != null) {
+			this.server.unregisterCallback(this);
+		}
 		this.server = server;
 		this.setSuccessor(server);
 		if (this.server != null) {
