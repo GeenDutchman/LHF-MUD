cfdc9eca9358e8c190a44b2fd8c1a57c002dfeb1
Drake Foltz <foltz.dw@gmail.com>
2023-12-29 14:57:44 -0700

    Doorway tests

--- gameserver/src/test/java/com/lhf/game/map/DoorwayTest.java
+++ gameserver/src/test/java/com/lhf/game/map/DoorwayTest.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.google.common.truth.Truth;
 import com.lhf.game.creature.intelligence.AIComBundle;
+import com.lhf.game.map.Dungeon.DungeonBuilder;
 import com.lhf.game.map.Room.RoomBuilder;
 import com.lhf.messages.MessageMatcher;
 import com.lhf.messages.GameEventType;
@@ -14,13 +16,17 @@ public class DoorwayTest {
         @Test
         void testCanTraverse() {
                 DungeonBuilder builder = DungeonBuilder.newInstance();
-                RoomBuilder rBuilder = RoomBuilder.getInstance();
-                Room roomA = rBuilder.setName("roomA").build();
-                Room roomB = rBuilder.setName("roomB").build();
-                builder.addStartingRoom(roomA);
-                builder.connectRoom(roomB, Directions.EAST, roomA);
-                Dungeon dungeon = builder.build();
+                RoomBuilder roomABuilder = RoomBuilder.getInstance().setName("roomA");
+                RoomBuilder roomBBuilder = RoomBuilder.getInstance().setName("roomB");
+                builder.addStartingRoom(roomABuilder);
+                builder.connectRoom(roomBBuilder, Directions.EAST, roomABuilder);
+                Dungeon dungeon = builder.quickBuild(null, null);
                 System.out.println(dungeon.toMermaid(false));
+
+                Area roomA = dungeon.getAreaByName("roomA").orElse(null);
+                Area roomB = dungeon.getAreaByName("roomB").orElse(null);
+                Truth.assertThat(roomA).isNotNull();
+                Truth.assertThat(roomB).isNotNull();
 
                 AIComBundle bundle = new AIComBundle();
                 roomA.addCreature(bundle.npc);
--- gameserver/src/test/java/com/lhf/game/map/KeyedDoorwayTest.java
+++ gameserver/src/test/java/com/lhf/game/map/KeyedDoorwayTest.java
@@ -1,6 +1,7 @@
 package com.lhf.game.map;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -8,31 +9,46 @@ import org.mockito.Mockito;
 import com.google.common.truth.Truth;
 import com.lhf.game.creature.intelligence.AIComBundle;
 import com.lhf.game.item.concrete.LockKey;
-import com.lhf.game.map.DoorwayFactory.DoorwayType;
-import com.lhf.game.map.Land.AreaDirectionalLinks;
+import com.lhf.game.map.Atlas.AtlasMappingItem;
+import com.lhf.game.map.Atlas.TargetedTester;
+import com.lhf.game.map.Dungeon.DungeonBuilder;
 import com.lhf.game.map.Room.RoomBuilder;
-import com.lhf.messages.MessageMatcher;
 import com.lhf.messages.GameEventType;
+import com.lhf.messages.MessageMatcher;
 
 public class KeyedDoorwayTest {
         @Test
         void testCanTraverse() {
                 DungeonBuilder builder = DungeonBuilder.newInstance();
-                RoomBuilder rBuilder = RoomBuilder.getInstance();
-                Room roomA = rBuilder.setName("roomA").build();
-                Room roomB = rBuilder.setName("roomB").build();
-                builder.addStartingRoom(roomA);
-                builder.connectRoom(DoorwayType.KEYED, roomB, Directions.EAST, roomA);
-                Dungeon dungeon = builder.build();
+                RoomBuilder roomABuilder = RoomBuilder.getInstance().setName("roomA");
+                RoomBuilder roomBBuilder = RoomBuilder.getInstance().setName("roomB");
+                builder.addStartingRoom(roomABuilder);
+                KeyedDoorway keyed = new KeyedDoorway();
+                builder.connectRoom(keyed, roomBBuilder, Directions.EAST, roomABuilder);
+                Dungeon dungeon = builder.quickBuild(null, null);
                 System.out.println(dungeon.toMermaid(false));
 
-                AreaDirectionalLinks exits = dungeon.getAreaDirectionalLinks(roomB);
-                Doorway genericDoor = exits.getExits().get(Directions.EAST);
-                Truth.assertThat(genericDoor.getRoomAccross(roomA.getUuid())).isEqualTo(roomB.getUuid());
-                Truth.assertThat(genericDoor.getRoomAccross(roomB.getUuid())).isEqualTo(roomA.getUuid());
-                Truth.assertThat(genericDoor.getType()).isEqualTo(DoorwayType.KEYED);
-                KeyedDoorway doorway = (KeyedDoorway) genericDoor;
-                Truth.assertThat(doorway.isOpen()).isFalse();
+                Area roomA = dungeon.getAreaByName("roomA").orElse(null);
+                Area roomB = dungeon.getAreaByName("roomB").orElse(null);
+                Truth.assertThat(roomA).isNotNull();
+                Truth.assertThat(roomB).isNotNull();
+
+                AtlasMappingItem<Area, UUID> aMappingItem = dungeon.getAtlas().getAtlasMappingItem(roomA);
+                AtlasMappingItem<Area, UUID> bMappingItem = dungeon.getAtlas().getAtlasMappingItem(roomB);
+
+                Truth.assertThat(bMappingItem.getAvailableDirections()).contains(Directions.EAST);
+                TargetedTester<UUID> aUUID = bMappingItem.getDirections().get(Directions.EAST);
+                Truth.assertThat(aUUID).isNotNull();
+                Truth.assertThat(aUUID.getTargetId()).isEqualTo(roomA.getUuid());
+                Truth.assertThat(aUUID.getPredicate()).isEqualTo(keyed);
+
+                Truth.assertThat(aMappingItem.getAvailableDirections()).contains(Directions.WEST);
+                TargetedTester<UUID> bUUID = aMappingItem.getDirections().get(Directions.WEST);
+                Truth.assertThat(bUUID).isNotNull();
+                Truth.assertThat(bUUID.getTargetId()).isEqualTo(roomB.getUuid());
+                Truth.assertThat(bUUID.getPredicate()).isEqualTo(keyed);
+
+                Truth.assertThat(keyed.isOpen()).isFalse();
 
                 AIComBundle bundle = new AIComBundle();
                 roomA.addCreature(bundle.npc);
@@ -45,10 +61,10 @@ public class KeyedDoorwayTest {
                 Mockito.verify(bundle.sssb, Mockito.timeout(1000))
                                 .send(Mockito.argThat(new MessageMatcher(GameEventType.BAD_GO, "blocked")));
 
-                LockKey key = doorway.generateKey();
+                LockKey key = keyed.generateKey();
                 bundle.npc.addItem(key);
                 Truth.assertThat(bundle.npc.hasItem(key.getName()));
-                Truth.assertThat(LockKey.generateKeyName(doorway.getLockUUID())).isEqualTo(key.getName());
+                Truth.assertThat(LockKey.generateKeyName(keyed.getLockUUID())).isEqualTo(key.getName());
 
                 bundle.brain.ProcessString("go west");
                 Mockito.verify(bundle.sssb, Mockito.timeout(1000))
@@ -57,7 +73,7 @@ public class KeyedDoorwayTest {
                                                 null)));
 
                 Truth.assertThat(bundle.npc.hasItem(key.getName())).isFalse();
-                Truth.assertThat(doorway.isOpen()).isTrue();
+                Truth.assertThat(keyed.isOpen()).isTrue();
 
                 bundle.brain.ProcessString("go east");
                 Mockito.verify(bundle.sssb, Mockito.timeout(1000).times(2))
