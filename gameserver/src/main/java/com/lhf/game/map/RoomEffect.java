ab9df5a919b7dbf6589c1be91cf374933df3efad
Drake Foltz <foltz.dw@gmail.com>
2023-12-29 21:20:36 -0700

    RoomEffect caches the values

--- gameserver/src/main/java/com/lhf/game/map/RoomEffect.java
+++ gameserver/src/main/java/com/lhf/game/map/RoomEffect.java
@@ -1,31 +1,71 @@
 package com.lhf.game.map;
 
+import java.io.FileNotFoundException;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
 import com.lhf.Taggable;
 import com.lhf.game.EntityEffect;
 import com.lhf.game.creature.ICreature;
+import com.lhf.game.creature.IMonster;
+import com.lhf.game.creature.INonPlayerCharacter;
 import com.lhf.game.creature.Monster.MonsterBuilder;
 import com.lhf.game.creature.NonPlayerCharacter.NPCBuilder;
+import com.lhf.game.creature.statblock.StatblockManager;
+import com.lhf.messages.CommandChainHandler;
+import com.lhf.server.client.CommandInvoker;
 import com.lhf.server.interfaces.NotNull;
 
 public class RoomEffect extends EntityEffect {
+    protected INonPlayerCharacter summonedNPC;
+    protected IMonster summonedMonster;
 
     public RoomEffect(@NotNull RoomEffect other) {
         super(other.source, other.creatureResponsible, other.generatedBy);
+        this.summonedMonster = null;
+        this.summonedNPC = null;
     }
 
     public RoomEffect(RoomEffectSource source, ICreature creatureResponsible, Taggable generatedBy) {
         super(source, creatureResponsible, generatedBy);
+        this.summonedMonster = null;
+        this.summonedNPC = null;
     }
 
     public RoomEffectSource getSource() {
         return (RoomEffectSource) this.source;
     }
 
-    public NPCBuilder getNPCToSummon() {
+    private NPCBuilder getNPCToSummon() {
         return this.getSource().getNpcToSummon();
     }
 
-    public MonsterBuilder getMonsterToSummon() {
+    private MonsterBuilder getMonsterToSummon() {
         return this.getSource().getMonsterToSummon();
+    }
+
+    public INonPlayerCharacter getSummonedNPC(Supplier<CommandInvoker> controllerSupplier,
+            CommandChainHandler successor, StatblockManager statblockManager,
+            UnaryOperator<NPCBuilder> composedlazyLoaders) throws FileNotFoundException {
+        if (this.summonedNPC == null) {
+            NPCBuilder builder = getNPCToSummon();
+            if (builder != null) {
+                this.summonedNPC = builder.build(controllerSupplier, successor, statblockManager, composedlazyLoaders);
+            }
+        }
+        return summonedNPC;
+    }
+
+    public IMonster getSummonedMonster(Supplier<CommandInvoker> controllerSupplier,
+            CommandChainHandler successor, StatblockManager statblockManager,
+            UnaryOperator<MonsterBuilder> composedlazyLoaders) throws FileNotFoundException {
+        if (this.summonedMonster == null) {
+            MonsterBuilder builder = getMonsterToSummon();
+            if (builder != null) {
+                this.summonedMonster = builder.build(controllerSupplier, successor, statblockManager,
+                        composedlazyLoaders);
+            }
+        }
+        return summonedMonster;
     }
 }
