438d9ec9a1094213cad3004791cb7441779029a6
Drake Foltz <foltz.dw@gmail.com>
2024-01-06 15:14:14 -0700

    intermediate step

--- gameserver/src/main/java/com/lhf/messages/in/CommandDispatcher.java
+++ gameserver/src/main/java/com/lhf/messages/in/CommandDispatcher.java
@@ -1,72 +1,87 @@
 package com.lhf.messages.in;
 
+import java.util.Map;
+import java.util.logging.Level;
+
 import com.lhf.messages.Command;
 import com.lhf.messages.CommandChainHandler;
 import com.lhf.messages.CommandContext;
 import com.lhf.messages.CommandMessage;
 import com.lhf.messages.PooledMessageChainHandler;
+import com.lhf.messages.CommandChainHandler.CommandHandler;
 
 public class CommandDispatcher {
-    public static CommandContext.Reply dispatch(CommandContext ctx, Command command, CommandChainHandler handler) {
-        if (ctx == null || command == null || handler == null) {
+    public static CommandContext.Reply dispatch(CommandContext ctx, Command command, CommandChainHandler chainHandler) {
+        if (ctx == null || command == null || chainHandler == null) {
             throw new IllegalArgumentException(
-                    String.format("Cannot use a null context, command, or handler: %s, %s, %s", ctx, command, handler));
+                    String.format("Cannot use a null context, command, or handler: %s, %s, %s", ctx, command,
+                            chainHandler));
         }
         CommandMessage commandType = command.getType();
         if (commandType == null) {
             throw new IllegalStateException(String.format("Command '%s' must not have a null type", command));
         }
+        chainHandler.addSelfToContext(ctx);
+        Map<CommandMessage, CommandHandler<?>> handlers = chainHandler.getCommands(ctx);
+        if (handlers == null) {
+            chainHandler.log(Level.FINEST,
+                    () -> String.format("No CommandHandler for type %s at this level", commandType));
+            return ctx.failhandle();
+        }
+
         switch (commandType) {
             case ATTACK:
-                return handler.handleInCommand(ctx, new AttackMessage(command));
+                CommandHandler<?> handler = handlers.get(commandType);
+                handler.handleCommand(ctx, command);
+                return chainHandler.handleCommand(ctx, new AttackMessage(command));
             case CAST:
-                return handler.handleInCommand(ctx, new CastMessage(command));
+                return chainHandler.handleCommand(ctx, new CastMessage(command));
             case CREATE:
-                return handler.handleInCommand(ctx, new CreateInMessage(command));
+                return chainHandler.handleCommand(ctx, new CreateInMessage(command));
             case DROP:
-                return handler.handleInCommand(ctx, new DropMessage(command));
+                return chainHandler.handleCommand(ctx, new DropMessage(command));
             case EQUIP:
-                return handler.handleInCommand(ctx, new EquipMessage(command));
+                return chainHandler.handleCommand(ctx, new EquipMessage(command));
             case EXIT:
-                return handler.handleInCommand(ctx, new ExitMessage(command));
+                return chainHandler.handleCommand(ctx, new ExitMessage(command));
             case FOLLOW:
-                return handler.handleInCommand(ctx, new FollowMessage(command));
+                return chainHandler.handleCommand(ctx, new FollowMessage(command));
             case GO:
-                return handler.handleInCommand(ctx, new GoMessage(command));
+                return chainHandler.handleCommand(ctx, new GoMessage(command));
             case HELP:
-                return handler.handleInCommand(ctx, new HelpInMessage(command));
+                return chainHandler.handleCommand(ctx, new HelpInMessage(command));
             case INTERACT:
-                return handler.handleInCommand(ctx, new InteractMessage(command));
+                return chainHandler.handleCommand(ctx, new InteractMessage(command));
             case INVENTORY:
-                return handler.handleInCommand(ctx, new InventoryMessage(command));
+                return chainHandler.handleCommand(ctx, new InventoryMessage(command));
             case LEWD:
-                return handler.handleInCommand(ctx, new LewdInMessage(command));
+                return chainHandler.handleCommand(ctx, new LewdInMessage(command));
             case PASS:
-                return handler.handleInCommand(ctx, new PassMessage(command));
+                return chainHandler.handleCommand(ctx, new PassMessage(command));
             case PLAYERS:
-                return handler.handleInCommand(ctx, new ListPlayersMessage(command));
+                return chainHandler.handleCommand(ctx, new ListPlayersMessage(command));
             case REPEAT:
-                return handler.handleInCommand(ctx, new RepeatInMessage(command));
+                return chainHandler.handleCommand(ctx, new RepeatInMessage(command));
             case REST:
-                return handler.handleInCommand(ctx, new RestMessage(command));
+                return chainHandler.handleCommand(ctx, new RestMessage(command));
             case SAY:
-                return handler.handleInCommand(ctx, new SayMessage(command));
+                return chainHandler.handleCommand(ctx, new SayMessage(command));
             case SEE:
-                return handler.handleInCommand(ctx, new SeeMessage(command));
+                return chainHandler.handleCommand(ctx, new SeeMessage(command));
             case SHOUT:
-                return handler.handleInCommand(ctx, new ShoutMessage(command));
+                return chainHandler.handleCommand(ctx, new ShoutMessage(command));
             case SPELLBOOK:
-                return handler.handleInCommand(ctx, new SpellbookMessage(command));
+                return chainHandler.handleCommand(ctx, new SpellbookMessage(command));
             case STATS:
-                return handler.handleInCommand(ctx, new StatsInMessage(command));
+                return chainHandler.handleCommand(ctx, new StatsInMessage(command));
             case STATUS:
-                return handler.handleInCommand(ctx, new StatusMessage(command));
+                return chainHandler.handleCommand(ctx, new StatusMessage(command));
             case TAKE:
-                return handler.handleInCommand(ctx, new TakeMessage(command));
+                return chainHandler.handleCommand(ctx, new TakeMessage(command));
             case UNEQUIP:
-                return handler.handleInCommand(ctx, new UnequipMessage(command));
+                return chainHandler.handleCommand(ctx, new UnequipMessage(command));
             case USE:
-                return handler.handleInCommand(ctx, new UseMessage(command));
+                return chainHandler.handleCommand(ctx, new UseMessage(command));
             default:
                 throw new UnsupportedOperationException(
                         String.format("Need adapter for command type %s", commandType.toString()));
@@ -87,55 +102,55 @@ public class CommandDispatcher {
         }
         switch (commandType) {
             case ATTACK:
-                return handler.flushHandleInCommand(ctx, new AttackMessage(command));
+                return handler.flushHandle(ctx, new AttackMessage(command));
             case CAST:
-                return handler.flushHandleInCommand(ctx, new CastMessage(command));
+                return handler.flushHandle(ctx, new CastMessage(command));
             case CREATE:
-                return handler.flushHandleInCommand(ctx, new CreateInMessage(command));
+                return handler.flushHandle(ctx, new CreateInMessage(command));
             case DROP:
-                return handler.flushHandleInCommand(ctx, new DropMessage(command));
+                return handler.flushHandle(ctx, new DropMessage(command));
             case EQUIP:
-                return handler.flushHandleInCommand(ctx, new EquipMessage(command));
+                return handler.flushHandle(ctx, new EquipMessage(command));
             case EXIT:
-                return handler.flushHandleInCommand(ctx, new ExitMessage(command));
+                return handler.flushHandle(ctx, new ExitMessage(command));
             case FOLLOW:
-                return handler.flushHandleInCommand(ctx, new FollowMessage(command));
+                return handler.flushHandle(ctx, new FollowMessage(command));
             case GO:
-                return handler.flushHandleInCommand(ctx, new GoMessage(command));
+                return handler.flushHandle(ctx, new GoMessage(command));
             case HELP:
-                return handler.flushHandleInCommand(ctx, new HelpInMessage(command));
+                return handler.flushHandle(ctx, new HelpInMessage(command));
             case INTERACT:
-                return handler.flushHandleInCommand(ctx, new InteractMessage(command));
+                return handler.flushHandle(ctx, new InteractMessage(command));
             case INVENTORY:
-                return handler.flushHandleInCommand(ctx, new InventoryMessage(command));
+                return handler.flushHandle(ctx, new InventoryMessage(command));
             case LEWD:
-                return handler.flushHandleInCommand(ctx, new LewdInMessage(command));
+                return handler.flushHandle(ctx, new LewdInMessage(command));
             case PASS:
-                return handler.flushHandleInCommand(ctx, new PassMessage(command));
+                return handler.flushHandle(ctx, new PassMessage(command));
             case PLAYERS:
-                return handler.flushHandleInCommand(ctx, new ListPlayersMessage(command));
+                return handler.flushHandle(ctx, new ListPlayersMessage(command));
             case REPEAT:
-                return handler.flushHandleInCommand(ctx, new RepeatInMessage(command));
+                return handler.flushHandle(ctx, new RepeatInMessage(command));
             case REST:
-                return handler.flushHandleInCommand(ctx, new RestMessage(command));
+                return handler.flushHandle(ctx, new RestMessage(command));
             case SAY:
-                return handler.flushHandleInCommand(ctx, new SayMessage(command));
+                return handler.flushHandle(ctx, new SayMessage(command));
             case SEE:
-                return handler.flushHandleInCommand(ctx, new SeeMessage(command));
+                return handler.flushHandle(ctx, new SeeMessage(command));
             case SHOUT:
-                return handler.flushHandleInCommand(ctx, new ShoutMessage(command));
+                return handler.flushHandle(ctx, new ShoutMessage(command));
             case SPELLBOOK:
-                return handler.flushHandleInCommand(ctx, new SpellbookMessage(command));
+                return handler.flushHandle(ctx, new SpellbookMessage(command));
             case STATS:
-                return handler.flushHandleInCommand(ctx, new StatsInMessage(command));
+                return handler.flushHandle(ctx, new StatsInMessage(command));
             case STATUS:
-                return handler.flushHandleInCommand(ctx, new StatusMessage(command));
+                return handler.flushHandle(ctx, new StatusMessage(command));
             case TAKE:
-                return handler.flushHandleInCommand(ctx, new TakeMessage(command));
+                return handler.flushHandle(ctx, new TakeMessage(command));
             case UNEQUIP:
-                return handler.flushHandleInCommand(ctx, new UnequipMessage(command));
+                return handler.flushHandle(ctx, new UnequipMessage(command));
             case USE:
-                return handler.flushHandleInCommand(ctx, new UseMessage(command));
+                return handler.flushHandle(ctx, new UseMessage(command));
             default:
                 throw new UnsupportedOperationException(
                         String.format("Need adapter for command type %s", commandType.toString()));
