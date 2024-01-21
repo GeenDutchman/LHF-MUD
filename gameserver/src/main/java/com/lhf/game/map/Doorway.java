9df7ac575a44f8dab2f3a3b42aa90b9c295a4bd1
Drake Foltz <foltz.dw@gmail.com>
2024-01-20 22:02:36 -0700

    give it a className

--- gameserver/src/main/java/com/lhf/game/map/Doorway.java
+++ gameserver/src/main/java/com/lhf/game/map/Doorway.java
@@ -4,13 +4,14 @@ import com.lhf.game.creature.ICreature;
 import com.lhf.game.map.Land.TraversalTester;
 
 class Doorway implements TraversalTester {
+    protected final String className;
 
     public enum DoorwayType {
         STANDARD, ONE_WAY, CLOSEABLE, KEYED;
     }
 
     public Doorway() {
-
+        this.className = this.getClass().getName();
     }
 
     public DoorwayType getType() {
@@ -20,6 +21,10 @@ class Doorway implements TraversalTester {
     @Override
     public boolean testTraversal(ICreature creature, Directions direction, Area source, Area dest) {
         return true;
+    }
+
+    public String getClassName() {
+        return className;
     }
 
 }
