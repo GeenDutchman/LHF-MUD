0bbec9c7a7d2d63afdd25ae2c453643a5b89c806
Drake Foltz <foltz.dw@gmail.com>
2024-03-13 21:03:18 -0600

    runs, but has wrong stats

--- gameserver/src/test/java/com/lhf/game/creature/CreatureBuildInfoTest.java
+++ gameserver/src/test/java/com/lhf/game/creature/CreatureBuildInfoTest.java
@@ -1,13 +1,22 @@
 package com.lhf.game.creature;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.google.common.reflect.TypeToken;
 import com.google.common.truth.Truth;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.lhf.game.creature.Player.PlayerBuildInfo;
 import com.lhf.game.creature.inventory.Inventory;
 import com.lhf.game.creature.statblock.AttributeBlock;
+import com.lhf.game.creature.vocation.Vocation.VocationName;
+import com.lhf.game.enums.CreatureFaction;
 import com.lhf.game.enums.EquipmentSlots;
 import com.lhf.game.enums.Stats;
 import com.lhf.game.item.AItem;
@@ -58,5 +67,48 @@ public class CreatureBuildInfoTest {
         Truth.assertThat(num2.getEquipmentSlots().getOrDefault(EquipmentSlots.WEAPON, null).produceMessage()
                 .toString())
                 .isEqualTo(s.getEquipmentSlots().getOrDefault(EquipmentSlots.WEAPON, null).produceMessage().toString());
+    }
+
+    @Test
+    void testPlayerSerialization() {
+        List<ICreatureBuildInfo> toBuild = new ArrayList<>();
+        PlayerBuildInfo buildInfo = PlayerBuildInfo.getInstance(null);
+        String name = NameGenerator.Generate(null);
+        buildInfo.setName(name);
+        buildInfo.setVocation(VocationName.HEALER);
+        toBuild.add(buildInfo);
+        Gson gson = GsonBuilderFactory.start().items().creatureInfoBuilders().prettyPrinting().build();
+        String json = gson.toJson(toBuild);
+        System.out.println(json);
+        Truth.assertThat(json).contains(name);
+    }
+
+    @Test
+    void testPlayerMinimumDeserialization() {
+        JsonArray list = new JsonArray();
+        JsonObject playerBuildInfo = new JsonObject();
+        playerBuildInfo.addProperty("className", PlayerBuildInfo.class.getName());
+        JsonObject creatureBuilder = new JsonObject();
+        creatureBuilder.addProperty("className", CreatureBuildInfo.class.getName());
+        creatureBuilder.addProperty("creatureRace", "Creature");
+        final String name = NameGenerator.Generate(null);
+        creatureBuilder.addProperty("name", name);
+        creatureBuilder.addProperty("faction", CreatureFaction.PLAYER.toString());
+        creatureBuilder.addProperty("vocation", VocationName.HEALER.toString());
+        playerBuildInfo.add("creatureBuilder", creatureBuilder);
+        list.add(playerBuildInfo);
+        final String json = list.toString();
+        System.out.println(json);
+        Truth.assertThat(json).contains(name);
+        Gson gson = GsonBuilderFactory.start().items().creatureInfoBuilders().prettyPrinting().build();
+        final Type listType = new TypeToken<List<ICreatureBuildInfo>>() {
+        }.getType();
+        List<ICreatureBuildInfo> buildInfos = gson.fromJson(json, listType);
+        System.out.println(buildInfos);
+
+        final String buildInfoJson = gson.toJson(buildInfos);
+        System.out.println(buildInfoJson);
+        Truth.assertThat(buildInfoJson).contains(name);
+
     }
 }
