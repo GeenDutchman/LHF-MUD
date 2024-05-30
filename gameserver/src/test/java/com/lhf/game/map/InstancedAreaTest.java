9969a2a2cbab5f44075748f6e498c12be3fa01f6
Drake Foltz <foltz.dw@gmail.com>
2024-05-30 09:49:44 -0600

    Test room removal

--- gameserver/src/test/java/com/lhf/game/map/InstancedAreaTest.java
+++ gameserver/src/test/java/com/lhf/game/map/InstancedAreaTest.java
@@ -70,4 +70,61 @@ public class InstancedAreaTest {
                     player2, player3);
         }
     }
+
+    @Test
+    void testRemoveCreature() {
+        InstancedArea.InstancedAreaBuilder builder = InstancedArea.getBuilder().setLimit(1);
+
+        InstancedArea area = builder.build(null, null, null, null, false);
+
+        final GameEventProcessorID id1 = new GameEventProcessorID();
+        final Player player1 = Mockito.mock(Player.class);
+        Mockito.when(player1.getName()).thenReturn("George");
+        Mockito.doAnswer(invocation -> {
+            CreatureVisitor visitor = invocation.getArgument(0);
+            Player myself = (Player) invocation.getMock();
+            visitor.visit(myself);
+            return null;
+        }).when(player1).acceptCreatureVisitor(Mockito.any(CreatureVisitor.class));
+        Mockito.when(player1.getEventProcessorID()).thenReturn(id1);
+        Mockito.when(player1.getAcceptHook()).thenReturn(
+                event -> System.out.println(String.format("Player %s: %s", player1.getName(), event.toString())));
+
+        final GameEventProcessorID id2 = new GameEventProcessorID();
+        final Player player2 = Mockito.mock(Player.class);
+        Mockito.when(player2.getName()).thenReturn("Mary");
+        Mockito.doAnswer(invocation -> {
+            CreatureVisitor visitor = invocation.getArgument(0);
+            Player myself = (Player) invocation.getMock();
+            visitor.visit(myself);
+            return null;
+        }).when(player2).acceptCreatureVisitor(Mockito.any(CreatureVisitor.class));
+        Mockito.when(player2.getEventProcessorID()).thenReturn(id2);
+        Mockito.when(player2.getAcceptHook()).thenReturn(
+                event -> System.out.println(String.format("Player %s: %s", player2.getName(), event.toString())));
+
+        final GameEventProcessorID id3 = new GameEventProcessorID();
+        final Player player3 = Mockito.mock(Player.class);
+        Mockito.when(player3.getName()).thenReturn("Beren");
+        Mockito.doAnswer(invocation -> {
+            CreatureVisitor visitor = invocation.getArgument(0);
+            Player myself = (Player) invocation.getMock();
+            visitor.visit(myself);
+            return null;
+        }).when(player3).acceptCreatureVisitor(Mockito.any(CreatureVisitor.class));
+        Mockito.when(player3.getEventProcessorID()).thenReturn(id3);
+        Mockito.when(player3.getAcceptHook()).thenReturn(
+                event -> System.out.println(String.format("Player %s: %s", player3.getName(), event.toString())));
+
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.addCreature(player1)).isTrue();
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.addCreature(player2)).isTrue();
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.addCreature(player3)).isTrue();
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.getRooms()).hasSize(3);
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.getCreatures()).containsExactly(player1, player2,
+                player3);
+
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.removeCreature(player3)).isTrue();
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.getRooms()).hasSize(2);
+        Truth.assertWithMessage("Area is such: '%s'", area).that(area.getCreatures()).doesNotContain(player3);
+    }
 }
